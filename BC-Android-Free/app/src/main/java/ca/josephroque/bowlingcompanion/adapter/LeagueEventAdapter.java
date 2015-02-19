9e72c41d7ee1a6b6b1480b49bf856a8abe139899
josephroque <github@josephroque.ca>
2015-02-19 13:24:51 -0500

    Implemented deleting leagues and events

--- BC-Android-Free/app/src/main/java/ca/josephroque/bowlingcompanion/adapter/LeagueEventAdapter.java
+++ BC-Android-Free/app/src/main/java/ca/josephroque/bowlingcompanion/adapter/LeagueEventAdapter.java
@@ -1,8 +1,12 @@
 package ca.josephroque.bowlingcompanion.adapter;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +16,8 @@ import java.util.List;
 
 import ca.josephroque.bowlingcompanion.Constants;
 import ca.josephroque.bowlingcompanion.R;
+import ca.josephroque.bowlingcompanion.database.Contract.*;
+import ca.josephroque.bowlingcompanion.database.DatabaseHelper;
 
 /**
  * Created by josephroque on 15-02-18.
@@ -25,41 +31,41 @@ public class LeagueEventAdapter extends RecyclerView.Adapter<LeagueEventAdapter.
 
     private Activity mActivity;
 
-    private List<Long> mListLeagueIds;
-    private List<String> mListLeagueNames;
-    private List<Short> mListLeagueAverages;
-    private List<Byte> mListLeagueNumberOfGames;
+    private List<Long> mListLeagueEventIds;
+    private List<String> mListLeagueEventNames;
+    private List<Short> mListLeagueEventAverages;
+    private List<Byte> mListLeagueEventNumberOfGames;
 
     public static class LeagueEventViewHolder extends RecyclerView.ViewHolder
     {
-        private TextView mTextViewLeagueName;
-        private TextView mTextViewLeagueAverage;
-        private TextView mTextViewLeagueNumberOfGames;
+        private TextView mTextViewLeagueEventName;
+        private TextView mTextViewLeagueEventAverage;
+        private TextView mTextViewLeagueEventNumberOfGames;
 
         private LeagueEventViewHolder(View itemLayoutView)
         {
             super(itemLayoutView);
-            mTextViewLeagueName = (TextView)
+            mTextViewLeagueEventName = (TextView)
                     itemLayoutView.findViewById(R.id.textView_league_event_name);
-            mTextViewLeagueAverage = (TextView)
+            mTextViewLeagueEventAverage = (TextView)
                     itemLayoutView.findViewById(R.id.textView_league_event_average);
-            mTextViewLeagueNumberOfGames = (TextView)
+            mTextViewLeagueEventNumberOfGames = (TextView)
                     itemLayoutView.findViewById(R.id.textView_league_event_games);
         }
     }
 
     public LeagueEventAdapter(
             Activity activity,
-            List<Long> listLeagueIds,
-            List<String> listLeagueNames,
-            List<Short> listLeagueAverages,
-            List<Byte> listLeagueNumberOfGames)
+            List<Long> listLeagueEventIds,
+            List<String> listLeagueEventNames,
+            List<Short> listLeagueEventAverages,
+            List<Byte> listLeagueEventNumberOfGames)
     {
         this.mActivity = activity;
-        this.mListLeagueIds = listLeagueIds;
-        this.mListLeagueNames = listLeagueNames;
-        this.mListLeagueAverages = listLeagueAverages;
-        this.mListLeagueNumberOfGames = listLeagueNumberOfGames;
+        this.mListLeagueEventIds = listLeagueEventIds;
+        this.mListLeagueEventNames = listLeagueEventNames;
+        this.mListLeagueEventAverages = listLeagueEventAverages;
+        this.mListLeagueEventNumberOfGames = listLeagueEventNumberOfGames;
     }
 
     @Override
@@ -73,9 +79,9 @@ public class LeagueEventAdapter extends RecyclerView.Adapter<LeagueEventAdapter.
     @Override
     public void onBindViewHolder(LeagueEventViewHolder holder, final int position)
     {
-        holder.mTextViewLeagueName.setText(mListLeagueNames.get(position));
-        holder.mTextViewLeagueAverage.setText(String.valueOf(mListLeagueAverages.get(position)));
-        holder.mTextViewLeagueNumberOfGames.setText(String.valueOf(mListLeagueNumberOfGames.get(position)));
+        holder.mTextViewLeagueEventName.setText(mListLeagueEventNames.get(position));
+        holder.mTextViewLeagueEventAverage.setText(String.valueOf(mListLeagueEventAverages.get(position)));
+        holder.mTextViewLeagueEventNumberOfGames.setText(String.valueOf(mListLeagueEventNumberOfGames.get(position)));
         holder.itemView.setBackgroundColor(
                 mActivity.getResources().getColor(R.color.secondary_background));
 
@@ -93,7 +99,7 @@ public class LeagueEventAdapter extends RecyclerView.Adapter<LeagueEventAdapter.
             @Override
             public boolean onLongClick(View v)
             {
-                //TODO: showDeleteLeagueDialog(position)
+                showDeleteLeagueOrEventDialog(position);
                 return true;
             }
         });
@@ -101,6 +107,86 @@ public class LeagueEventAdapter extends RecyclerView.Adapter<LeagueEventAdapter.
 
     public int getItemCount()
     {
-        return mListLeagueIds.size();
+        return mListLeagueEventIds.size();
+    }
+
+    private void showDeleteLeagueOrEventDialog(final int position)
+    {
+        final String leagueName = mListLeagueEventNames.get(position);
+        final long leagueID = mListLeagueEventIds.get(position);
+
+        if (leagueName.equals(Constants.NAME_LEAGUE_OPEN))
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+            builder.setMessage("The league \"" + leagueName + "\" cannot be deleted.")
+                    .setCancelable(false)
+                    .setPositiveButton(Constants.DIALOG_OKAY, new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create()
+                    .show();
+            return;
+        }
+
+        DatabaseHelper.deleteData(mActivity,
+                new DatabaseHelper.DataDeleter()
+                {
+                    @Override
+                    public void execute()
+                    {
+                        deleteLeague(leagueID);
+                    }
+                },
+                leagueName);
+    }
+
+    private void deleteLeague(final long selectedLeagueID)
+    {
+        final int indexOfId = mListLeagueEventIds.indexOf(selectedLeagueID);
+        final String leagueName = mListLeagueEventNames.remove(indexOfId);
+        mListLeagueEventAverages.remove(indexOfId);
+        mListLeagueEventNumberOfGames.remove(indexOfId);
+        mListLeagueEventIds.remove(indexOfId);
+        notifyItemRemoved(indexOfId);
+
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                SQLiteDatabase database = DatabaseHelper.getInstance(mActivity).getWritableDatabase();
+                String[] whereArgs = {String.valueOf(selectedLeagueID)};
+                database.beginTransaction();
+                try
+                {
+                    database.delete(FrameEntry.TABLE_NAME,
+                            FrameEntry.COLUMN_NAME_LEAGUE_ID + "=?",
+                            whereArgs);
+                    database.delete(GameEntry.TABLE_NAME,
+                            GameEntry.COLUMN_NAME_LEAGUE_ID + "=?",
+                            whereArgs);
+                    database.delete(SeriesEntry.TABLE_NAME,
+                            SeriesEntry.COLUMN_NAME_LEAGUE_ID + "=?",
+                            whereArgs);
+                    database.delete(LeagueEntry.TABLE_NAME,
+                            LeagueEntry._ID + "=?",
+                            whereArgs);
+                    database.setTransactionSuccessful();
+                }
+                catch (Exception e)
+                {
+                    Log.w(TAG, "Error deleting league: " + leagueName);
+                }
+                finally
+                {
+                    database.endTransaction();
+                }
+            }
+        }).start();
     }
 }
