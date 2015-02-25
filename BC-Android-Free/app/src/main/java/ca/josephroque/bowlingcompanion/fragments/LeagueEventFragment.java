8e4e11764ad86f13007aaab6d589f20e25e269c7
Joseph Roque <github@josephroque.ca>
2015-02-25 18:01:56 -0500

    Added documentation to LeagueEventFragment.java

--- BC-Android-Free/app/src/main/java/ca/josephroque/bowlingcompanion/fragments/LeagueEventFragment.java
+++ BC-Android-Free/app/src/main/java/ca/josephroque/bowlingcompanion/fragments/LeagueEventFragment.java
@@ -28,7 +28,6 @@ import java.util.List;
 
 import ca.josephroque.bowlingcompanion.Constants;
 import ca.josephroque.bowlingcompanion.DividerItemDecoration;
-import ca.josephroque.bowlingcompanion.LeagueEventActivity;
 import ca.josephroque.bowlingcompanion.R;
 import ca.josephroque.bowlingcompanion.adapter.LeagueEventAdapter;
 import ca.josephroque.bowlingcompanion.database.Contract.*;
@@ -44,18 +43,33 @@ import ca.josephroque.bowlingcompanion.dialog.NewLeagueEventDialog;
 public class LeagueEventFragment extends Fragment
 {
 
+    /** Tag to identify class when outputting to console */
     private static final String TAG = "LeagueEventFragment";
 
+    /** Displays a list of items which each represent a league or event to the user */
     private RecyclerView mLeagueEventRecycler;
+    /** Manages the data for mLeagueEventRecycler and provides the items to display */
     private RecyclerView.Adapter mLeagueEventAdapter;
+    /** Displays an instructional message if no leagues or events exist */
     private TextView mNewLeagueEventInstructionsTextView;
 
+    /** Unique id of bowler selected by the user */
     private long mBowlerId = -1;
+    /** List of league ids from "league" table in database to uniquely identify leagues */
     private List<Long> mListLeagueEventIds;
+    /** List of league names which will be displayed by mLeagueEventRecycler */
     private List<String> mListLeagueEventNames;
+    /** List of league average which will be displayed by mLeagueEventRecycler */
     private List<Short> mListLeagueEventAverages;
+    /** List of number of games in leagues so series with proper number of games can be created */
     private List<Byte> mListLeagueEventNumberOfGames;
 
+    /**
+     * Creates a new instance of this fragment to display either leagues or events and returns it
+     *
+     * @param eventMode Indicates whether this object will be used to display leagues or events
+     * @return new instance of this object
+     */
     public static LeagueEventFragment newInstance(boolean eventMode)
     {
         LeagueEventFragment leagueEventFragment = new LeagueEventFragment();
@@ -69,6 +83,9 @@ public class LeagueEventFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance)
     {
+        /*
+         * Initializes member variables and obtains references to objects from root view.
+         */
         View rootView = inflater.inflate(R.layout.fragment_leagues_events, container, false);
 
         mListLeagueEventIds = new ArrayList<>();
@@ -128,6 +145,9 @@ public class LeagueEventFragment extends Fragment
         new LoadLeaguesEventsTask().execute();
     }
 
+    /**
+     * Prompts the user to add a new league or event
+     */
     private void showNewLeagueOrEventDialog()
     {
         DialogFragment dialog = new NewLeagueEventDialog();
@@ -137,11 +157,24 @@ public class LeagueEventFragment extends Fragment
         dialog.show(getActivity().getSupportFragmentManager(), "NewLeagueEventDialogFragment");
     }
 
+    /**
+     * Returns a boolean to indicate whether this instance of the fragment
+     * is intended to display leagues or events
+     *
+     * @return true if it is intended to display events, false otherwise
+     */
     private boolean isEventMode()
     {
         return getArguments().getBoolean(Constants.EXTRA_EVENT_MODE);
     }
 
+    /**
+     * Validates user input to create a new league/event entry in the database
+     * if the input is valid
+     *
+     * @param leagueName name of the league/event to be created
+     * @param numberOfGames number of games in a series in the league/event
+     */
     public void addNewLeagueOrEvent(String leagueName, byte numberOfGames)
     {
         boolean validInput = true;
@@ -151,6 +184,7 @@ public class LeagueEventFragment extends Fragment
                 (numberOfGames > Constants.MAX_NUMBER_LEAGUE_GAMES && !isEventMode())
                 || (numberOfGames > Constants.MAX_NUMBER_EVENT_GAMES && isEventMode()))
         {
+            //User has provided an invalid number of games
             validInput = false;
             invalidInputMessage = "The number of games must be between 1 and "
                     + (isEventMode()
@@ -160,17 +194,23 @@ public class LeagueEventFragment extends Fragment
         }
         else if (leagueName.equals(Constants.NAME_LEAGUE_OPEN))
         {
+            /*
+             * User has attempted to create a league or event entitled "Open"
+             * which is a reserved name
+             */
             validInput = false;
             invalidInputMessage = "That name is unavailable. It is a default used by the system. You must choose another.";
         }
         else if (mListLeagueEventNames.contains(leagueName))
         {
+            //User has provided a name which is already in use for a league or event
             validInput = false;
             invalidInputMessage = "That name has already been used. You must choose another.";
         }
 
         if (!validInput)
         {
+            //Displays an error dialog if the input was not valid and exits the method
             AlertDialog.Builder invalidInputBuilder = new AlertDialog.Builder(getActivity());
             invalidInputBuilder.setMessage(invalidInputMessage)
                     .setCancelable(false)
@@ -190,6 +230,10 @@ public class LeagueEventFragment extends Fragment
         new AddNewLeagueEventTask().execute(leagueName, numberOfGames);
     }
 
+    /**
+     * Loads the names of relevant leagues or events and adds them to the lists
+     * to be displayed to the user
+     */
     private class LoadLeaguesEventsTask extends AsyncTask<Void, Void, Void>
     {
         @Override
@@ -237,16 +281,24 @@ public class LeagueEventFragment extends Fragment
             mLeagueEventAdapter.notifyDataSetChanged();
             if (mListLeagueEventIds.size() > 0)
             {
+                /*
+                 * If there is at least one item loaded, the instructional text can be hidden
+                 */
                 mNewLeagueEventInstructionsTextView.setVisibility(View.GONE);
             }
         }
     }
 
+    /**
+     * Creates a new entry in the database for a league or event which is then added to the list
+     * of data to be displayed to the user.
+     */
     private class AddNewLeagueEventTask extends AsyncTask<Object, Void, Void>
     {
         @Override
         protected Void doInBackground(Object... params)
         {
+            //Gets name of league and the number of games from the parameters
             String leagueName = params[0].toString();
             byte numberOfGames = (Byte)params[1];
 
@@ -264,10 +316,15 @@ public class LeagueEventFragment extends Fragment
             database.beginTransaction();
             try
             {
+                //Creates the entry for the league or event in the "league" database
                 newId = database.insert(LeagueEntry.TABLE_NAME, null, values);
 
                 if (isEventMode())
                 {
+                    /*
+                     * If the new entry is an event, its series is also created at this time
+                     * since there is only a single series to an event
+                     */
                     values = new ContentValues();
                     values.put(SeriesEntry.COLUMN_NAME_DATE_CREATED, currentDate);
                     values.put(SeriesEntry.COLUMN_NAME_LEAGUE_ID, newId);
@@ -319,6 +376,7 @@ public class LeagueEventFragment extends Fragment
         @Override
         protected void onPostExecute(Void param)
         {
+            //Updates the data adapter and hides the instructional text
             mLeagueEventAdapter.notifyItemInserted(0);
             mNewLeagueEventInstructionsTextView.setVisibility(View.GONE);
         }
