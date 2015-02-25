3814e310e6c4d2ba228e9b48529193549bdd7120
Joseph Roque <github@josephroque.ca>
2015-02-25 14:47:01 -0500

    Added documentation to BowlerAdapter.java

--- BC-Android-Free/app/src/main/java/ca/josephroque/bowlingcompanion/adapter/BowlerAdapter.java
+++ BC-Android-Free/app/src/main/java/ca/josephroque/bowlingcompanion/adapter/BowlerAdapter.java
@@ -33,19 +33,37 @@ import ca.josephroque.bowlingcompanion.database.DatabaseHelper;
  */
 public class BowlerAdapter extends RecyclerView.Adapter<BowlerAdapter.BowlerViewHolder>
 {
+    /** Tag to identify class when outputting to console */
     private static final String TAG = "BowlerAdapter";
 
+    /** Instance of activity which created instance of this object */
     private Activity mActivity;
+    /** List of bowler names which will be displayed by RecyclerView */
     private List<String> mBowlerNames;
+    /** List of bowler averages which will be displayed by RecyclerView */
     private List<Short> mBowlerAverages;
-    private List<Long> mBowlerIDs;
+    /** List of bowler Ids from "bowler" table in database to uniquely identify bowlers */
+    private List<Long> mBowlerIds;
 
+    /**
+     * Subclass of RecyclerView.ViewHolder to manage views which will display an
+     * image and text to user.
+     */
     public static class BowlerViewHolder extends RecyclerView.ViewHolder
     {
+        /** Displays an image representing type of bowler*/
         private ImageView mImageViewBowlerOrTeam;
+        /** Displays name of bowler */
         private TextView mTextViewBowlerName;
+        /** Displays average of bowler */
         private TextView mTextViewBowlerAverage;
 
+        /**
+         * Calls super constructor with itemLayoutView as parameter and retrieves
+         * references to ImageView and TextView for member variables
+         *
+         * @param itemLayoutView
+         */
         private BowlerViewHolder(View itemLayoutView)
         {
             super(itemLayoutView);
@@ -55,10 +73,18 @@ public class BowlerAdapter extends RecyclerView.Adapter<BowlerAdapter.BowlerView
         }
     }
 
-    public BowlerAdapter(Activity context, List<Long> bowlerIDs, List<String> bowlerNames, List<Short> bowlerAverages)
+    /**
+     * Stores references to parameters in member variables
+     *
+     * @param context activity which created this object
+     * @param bowlerIds list of unique bowler ids from "bowler" table in database
+     * @param bowlerNames list of bowler names which correspond to order of ids from bowlerIds
+     * @param bowlerAverages list of bowler averages which correspond to order of ids from bowlerIds
+     */
+    public BowlerAdapter(Activity context, List<Long> bowlerIds, List<String> bowlerNames, List<Short> bowlerAverages)
     {
         mActivity = context;
-        this.mBowlerIDs = bowlerIDs;
+        this.mBowlerIds = bowlerIds;
         this.mBowlerNames = bowlerNames;
         this.mBowlerAverages = bowlerAverages;
     }
@@ -75,6 +101,10 @@ public class BowlerAdapter extends RecyclerView.Adapter<BowlerAdapter.BowlerView
     @Override
     public void onBindViewHolder(BowlerViewHolder holder, final int position)
     {
+        /*
+         * Sets image and text to represent individual bowler in list
+         * to display to user.
+         */
         holder.mImageViewBowlerOrTeam.setImageResource(R.drawable.ic_person);
         holder.mTextViewBowlerName.setText(mBowlerNames.get(position));
         holder.mTextViewBowlerAverage.setText("Avg: "
@@ -83,6 +113,10 @@ public class BowlerAdapter extends RecyclerView.Adapter<BowlerAdapter.BowlerView
         holder.itemView.setBackgroundColor(
                 mActivity.getResources().getColor(R.color.secondary_background));
 
+        /*
+         * Below methods are executed when an item in the RecyclerView is
+         * clicked or long clicked.
+         */
         holder.itemView.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -109,10 +143,16 @@ public class BowlerAdapter extends RecyclerView.Adapter<BowlerAdapter.BowlerView
         return mBowlerNames.size();
     }
 
+    /**
+     * Prompts user with a dialog to delete all data regarding a certain
+     * bowler in the database
+     *
+     * @param position position of bowler id in mBowlerIds
+     */
     private void showDeleteBowlerDialog(final int position)
     {
         final String bowlerName = mBowlerNames.get(position);
-        final long bowlerID = mBowlerIDs.get(position);
+        final long bowlerID = mBowlerIds.get(position);
 
         DatabaseHelper.deleteData(mActivity,
                 new DatabaseHelper.DataDeleter()
@@ -126,13 +166,23 @@ public class BowlerAdapter extends RecyclerView.Adapter<BowlerAdapter.BowlerView
                 bowlerName);
     }
 
+    /**
+     * Deletes all data regarding a certain bowlerId in the database
+     *
+     * @param selectedBowlerID id of bowler whose data will be deleted
+     */
     private void deleteBowler(final long selectedBowlerID)
     {
-        final int indexOfId = mBowlerIDs.indexOf(selectedBowlerID);
+        //Removes bowler from RecyclerView immediately so UI does not hang
+        final int indexOfId = mBowlerIds.indexOf(selectedBowlerID);
         final String bowlerName = mBowlerNames.remove(indexOfId);
-        mBowlerIDs.remove(indexOfId);
+        mBowlerIds.remove(indexOfId);
         notifyItemRemoved(indexOfId);
 
+        /*
+         * Gets recentBowlerId and quickBowlerId to check if the deleted bowler was either.
+         * If so, the value is cleared.
+         */
         SharedPreferences preferences = mActivity.getSharedPreferences(Constants.PREFERENCES, Activity.MODE_PRIVATE);
         long recentBowlerId = preferences.getLong(Constants.PREFERENCE_ID_RECENT_BOWLER, -1);
         long quickBowlerId = preferences.getLong(Constants.PREFERENCE_ID_QUICK_BOWLER, -1);
@@ -155,11 +205,13 @@ public class BowlerAdapter extends RecyclerView.Adapter<BowlerAdapter.BowlerView
         }
         MainActivity.sQuickSeriesButtonEnabled = !(recentBowlerId == -1) || !(quickBowlerId == -1);
 
+        //Deletes data on a new thread so UI does not hang
         new Thread(new Runnable()
         {
             @Override
             public void run()
             {
+                //Deletes data from all tables corresponding to selectedBowlerId
                 SQLiteDatabase database = DatabaseHelper.getInstance(mActivity).getWritableDatabase();
                 String[] whereArgs = {String.valueOf(selectedBowlerID)};
                 database.beginTransaction();
@@ -194,18 +246,24 @@ public class BowlerAdapter extends RecyclerView.Adapter<BowlerAdapter.BowlerView
         }).start();
     }
 
+    /**
+     * Creates a LeagueEventActivity to displays the leagues and events corresponding
+     * to a bowler id selected by the user from the list displayed
+     */
     private class OpenBowlerLeaguesTask extends AsyncTask<Integer, Void, Void>
     {
         @Override
         protected Void doInBackground(Integer... position)
         {
-            Long selectedBowlerID = mBowlerIDs.get(position[0]);
+            //Gets id of bowler which was selected from position of selected item in list
+            Long selectedBowlerID = mBowlerIds.get(position[0]);
 
             SQLiteDatabase database = DatabaseHelper.getInstance(mActivity).getWritableDatabase();
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             ContentValues values = new ContentValues();
             values.put(BowlerEntry.COLUMN_NAME_DATE_MODIFIED, dateFormat.format(new Date()));
 
+            //Updates bowler row in database to be most recently edited
             database.beginTransaction();
             try
             {
