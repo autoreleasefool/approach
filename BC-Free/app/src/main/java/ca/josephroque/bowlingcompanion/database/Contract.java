f556ac6a01573f5f9ff494878cbae4dfcaee128d
Joseph Roque <github@josephroque.ca>
2015-03-13 13:31:27 -0400

    Created database helper and methods

---
+++ BC-Free/app/src/main/java/ca/josephroque/bowlingcompanion/database/Contract.java
@@ -0,0 +1,127 @@
+package ca.josephroque.bowlingcompanion.database;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by josephroque on 15-03-12.
+ * <p/>
+ * Location ca.josephroque.bowlingcompanion.database
+ * in project Bowling Companion
+ */
+public class Contract
+{
+
+    /**
+     * Private constructor, class cannot be instantiated
+     */
+    private Contract(){}
+
+    /**
+     * Table and column names for SQLite table relevant to bowlers
+     */
+    public static final class BowlerEntry implements BaseColumns
+    {
+        /**
+         * Private constructor, class cannot be instantiated
+         */
+        private BowlerEntry(){}
+
+        /** Name of the table for bowler data */
+        public static final String TABLE_NAME = "bowlers";
+        /** Name of the column for bowler names */
+        public static final String COLUMN_BOWLER_NAME = "bowler_name";
+        /** Name of the column for bowlers' most recent date modified */
+        public static final String COLUMN_DATE_MODIFIED = "bowler_date_modified";
+    }
+
+    /**
+     * Table and column names for SQLite table relevant to leagues
+     */
+    public static final class LeagueEntry implements BaseColumns
+    {
+        /**
+         * Private constructor, class cannot be instantiated
+         */
+        private LeagueEntry(){}
+
+        /** Name of the table for league data */
+        public static final String TABLE_NAME = "leagues";
+        /** Name of the column for league names */
+        public static final String COLUMN_LEAGUE_NAME = "league_name";
+        /** Name of the column for the number of games in a league */
+        public static final String COLUMN_NUMBER_OF_GAMES = "league_number_of_games";
+        /** Name of the column for the leagues' most recent date modified */
+        public static final String COLUMN_DATE_MODIFIED = "league_date_modified";
+        /** Name of the column to indicate if the row is an event */
+        public static final String COLUMN_IS_EVENT = "league_is_event";
+        /** Name of the column for foreign key to a bowler id */
+        public static final String COLUMN_BOWLER_ID = "league_bowler_id_fk";
+    }
+
+    /**
+     * Table and column names for SQLite table relevant to series
+     */
+    public static final class SeriesEntry implements BaseColumns
+    {
+        /**
+         * Private constructor, class cannot be instantiated
+         */
+        private SeriesEntry(){}
+
+        /** Name of the table for series data */
+        public static final String TABLE_NAME = "series";
+        /** Name of the table for the date of the series */
+        public static final String COLUMN_SERIES_DATE = "series_date";
+        /** Name of the table for foreign key to a league id */
+        public static final String COLUMN_LEAGUE_ID = "series_league_id_fk";
+    }
+
+    /**
+     * Table and column names for SQLite table relevant to games
+     */
+    public static final class GameEntry implements BaseColumns
+    {
+        /**
+         * Private constructor, class cannot be instantiated
+         */
+        private GameEntry(){}
+
+        /** Name of the table for game data */
+        public static final String TABLE_NAME = "games";
+        /** Name of the column for game number in a series */
+        public static final String COLUMN_GAME_NUMBER = "game_number";
+        /** Name of the column for the game's score */
+        public static final String COLUMN_SCORE = "game_score";
+        /** Name of the column to indicate if a game is locked from editing */
+        public static final String COLUMN_IS_LOCKED = "game_is_locked";
+        /** Name of the column to indicate if a game's score was manually set */
+        public static final String COLUMN_IS_MANUAL = "game_is_manual";
+        /** Name of the column for foreign key to a series id */
+        public static final String COLUMN_SERIES_ID = "game_series_id_fk";
+    }
+
+    /**
+     * Table and column names for SQLite table relevant to frames
+     */
+    public static final class FrameEntry implements BaseColumns
+    {
+        /**
+         * Private constructor, class cannot be instantiated
+         */
+        private FrameEntry(){}
+
+        /** Name of the table for frame data */
+        public static final String TABLE_NAME = "frames";
+        /** Name of the column for the frame number in a game */
+        public static final String COLUMN_FRAME_NUMBER = "frame_number";
+        /** Name of the column to indicate if a frame has been accessed */
+        public static final String COLUMN_IS_ACCESSED = "frame_accessed";
+        /** Name of the column to indicate the state of the pins in a frame */
+        public static final String[] COLUMN_PIN_STATE =
+                {"frame_pins_1", "frame_pins_2", "frame_pins_3"};
+        /** Name of the column to indicate if fouls were invoked in a frame */
+        public static final String COLUMN_FOULS = "frame_fouls";
+        /** Name of the column for foreign key to a game id */
+        public static final String COLUMN_GAME_ID = "frame_game_id_fk";
+    }
+}
---
+++ BC-Free/app/src/main/java/ca/josephroque/bowlingcompanion/database/DatabaseHelper.java
@@ -0,0 +1,181 @@
+package ca.josephroque.bowlingcompanion.database;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import ca.josephroque.bowlingcompanion.database.Contract.*;
+
+/**
+ * Created by josephroque on 15-03-12.
+ * <p/>
+ * Location ca.josephroque.bowlingcompanion.database
+ * in project Bowling Companion
+ */
+public class DatabaseHelper extends SQLiteOpenHelper
+{
+    /** Tag for the database to be used in log output */
+    private static final String TAG = "DBHelper";
+    /** Name of the database */
+    private static final String DATABASE_NAME = "bowlingdata";
+    /** Version of the database, incremented with changes */
+    private static final int DATABASE_VERSION = 1;
+
+    /** Instance of the database */
+    private SQLiteDatabase mDatabase;
+
+    /** Singleton instance of the DatabaseHelper */
+    private static DatabaseHelper sDatabaseHelperInstance = null;
+
+    /**
+     * Returns a singleton instance of DatabaseHelper
+     *
+     * @param context the current activity
+     * @return static instance of DatabaseHelper
+     */
+    public static DatabaseHelper getInstance(Context context)
+    {
+        if (sDatabaseHelperInstance == null)
+        {
+            sDatabaseHelperInstance = new DatabaseHelper(context);
+        }
+        return sDatabaseHelperInstance;
+    }
+
+    /**
+     * Private constructor for singleton access
+     *
+     * @param context the current activity
+     */
+    private DatabaseHelper(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        mDatabase = db;
+
+        /*
+         * Defines tables for the database, creating columns and constraints
+         */
+
+        mDatabase.execSQL("CREATE TABLE "
+                + BowlerEntry.TABLE_NAME + "("
+                + BowlerEntry._ID + " INTEGER PRIMARY KEY, "
+                + BowlerEntry.COLUMN_BOWLER_NAME + " TEXT NOT NULL COLLATE NOCASE, "
+                + BowlerEntry.COLUMN_DATE_MODIFIED + " TEXT NOT NULL"
+                + ");");
+        mDatabase.execSQL("CREATE TABLE "
+                + LeagueEntry.TABLE_NAME + "("
+                + LeagueEntry._ID + " INTEGER PRIMARY KEY, "
+                + LeagueEntry.COLUMN_LEAGUE_NAME + " TEXT NOT NULL COLLATE NOCASE, "
+                + LeagueEntry.COLUMN_NUMBER_OF_GAMES + " INTEGER NOT NULL, "
+                + LeagueEntry.COLUMN_DATE_MODIFIED + " TEXT NOT NULL, "
+                + LeagueEntry.COLUMN_IS_EVENT + " INTEGER NOT NULL DEFAULT 0, "
+                + LeagueEntry.COLUMN_BOWLER_ID + " INTEGER NOT NULL, "
+                + "CHECK (" + LeagueEntry.COLUMN_NUMBER_OF_GAMES + " > 0 AND " + LeagueEntry.COLUMN_NUMBER_OF_GAMES + " <= 20), "
+                + "CHECK (" + LeagueEntry.COLUMN_IS_EVENT + " = 0 OR " + LeagueEntry.COLUMN_IS_EVENT + " = 1)"
+                + ");");
+        mDatabase.execSQL("CREATE TABLE "
+                + SeriesEntry.TABLE_NAME + " ("
+                + SeriesEntry._ID + " INTEGER PRIMARY KEY, "
+                + SeriesEntry.COLUMN_SERIES_DATE + " TEXT NOT NULL, "
+                + SeriesEntry.COLUMN_LEAGUE_ID + " INTEGER NOT NULL"
+                + ");");
+        mDatabase.execSQL("CREATE TABLE "
+                + GameEntry.TABLE_NAME + " ("
+                + GameEntry._ID + " INTEGER PRIMARY KEY, "
+                + GameEntry.COLUMN_GAME_NUMBER + " INTEGER NOT NULL, "
+                + GameEntry.COLUMN_SCORE + " INTEGER NOT NULL DEFAULT 0, "
+                + GameEntry.COLUMN_IS_MANUAL + " INTEGER NOT NULL DEFAULT 0, "
+                + GameEntry.COLUMN_IS_LOCKED + " INTEGER NOT NULL DEFAULT 0, "
+                + GameEntry.COLUMN_SERIES_ID + " INTEGER NOT NULL, "
+                + "CHECK (" + GameEntry.COLUMN_GAME_NUMBER + " >= 1 AND " + GameEntry.COLUMN_GAME_NUMBER + " <= 20), "
+                + "CHECK (" + GameEntry.COLUMN_IS_LOCKED + " = 0 OR " + GameEntry.COLUMN_IS_LOCKED + " = 1), "
+                + "CHECK (" + GameEntry.COLUMN_IS_MANUAL + " = 0 OR " + GameEntry.COLUMN_IS_MANUAL + " = 1), "
+                + "CHECK (" + GameEntry.COLUMN_SCORE + " >= 0 OR " + GameEntry.COLUMN_SCORE + " <= 450)"
+                + ");");
+        mDatabase.execSQL("CREATE TABLE "
+                + FrameEntry.TABLE_NAME + " ("
+                + FrameEntry._ID + " INTEGER PRIMARY KEY, "
+                + FrameEntry.COLUMN_FRAME_NUMBER + " INTEGER NOT NULL, "
+                + FrameEntry.COLUMN_IS_ACCESSED + " INTEGER NOT NULL DEFAULT 0, "
+                + FrameEntry.COLUMN_PIN_STATE[0] + " TEXT NOT NULL DEFAULT '00000', "
+                + FrameEntry.COLUMN_PIN_STATE[1] + " TEXT NOT NULL DEFAULT '00000', "
+                + FrameEntry.COLUMN_PIN_STATE[2] + " TEXT NOT NULL DEFAULT '00000', "
+                + FrameEntry.COLUMN_FOULS + " TEXT NOT NULL DEFAULT '0', "
+                + FrameEntry.COLUMN_GAME_ID + " INTEGER NOT NULL, "
+                + "CHECK (" + FrameEntry.COLUMN_FRAME_NUMBER + " >= 1 AND " + FrameEntry.COLUMN_FRAME_NUMBER + " <= 10), "
+                + "CHECK (" + FrameEntry.COLUMN_IS_ACCESSED + " = 0 OR " + FrameEntry.COLUMN_IS_ACCESSED + " = 1)"
+                + ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        /**
+         * If an older version of the database exists, all the tables and data are dropped
+         * and the table is recreated.
+         *
+         * In future version, if database is updated then tables should be altered,
+         * not dropped
+         */
+        Log.w(TAG, "Upgrading database from version" + oldVersion + " to " + newVersion);
+        db.execSQL("DROP TABLE IF EXISTS " + FrameEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + GameEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + SeriesEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + LeagueEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + BowlerEntry.TABLE_NAME);
+        onCreate(db);
+    }
+
+    /**
+     * Displays a dialog to the user to delete data in the database
+     *
+     * @param context context instance to create dialog
+     * @param deleter interface which should be overridden to call relevant data deletion method
+     * @param name identifier for data to be deleted
+     */
+    public static void deleteData(final Context context, final DataDeleter deleter, final String name)
+    {
+        AlertDialog.Builder deleteBuilder = new AlertDialog.Builder(context);
+        deleteBuilder.setMessage("WARNING: This action cannot be undone! Delete all data for " + name + "?")
+                .setPositiveButton("Delete", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        deleter.execute();
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        dialog.dismiss();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+    /**
+     * Provides a method which can be overridden to delete specific data
+     * if the deleteData method is successful (user selects 'Delete')
+     */
+    public static interface DataDeleter
+    {
+        /**
+         * Must be overriden to provide access to a relevant method which
+         * should be used to delete data from the database
+         */
+        public void execute();
+    }
+}
