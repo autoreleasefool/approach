fab1e308ada80369a5f368499019185c2e2a42a4
Joseph Roque <github@josephroque.ca>
2015-03-13 22:27:20 -0400

    Layout changes

--- BC-Free/app/src/main/java/ca/josephroque/bowlingcompanion/MainActivity.java
+++ BC-Free/app/src/main/java/ca/josephroque/bowlingcompanion/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends ActionBarActivity
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -28,15 +27,11 @@ public class MainActivity extends ActionBarActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
+        switch(item.getItemId())
         {
-            return true;
+            case R.id.action_settings:
+                //TODO: show settings
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
