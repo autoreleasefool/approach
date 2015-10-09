dbd5970feae202725287b996140e861ba6e6564b
Joseph Roque <contact@josephroque.ca>
2015-10-09 11:53:26 -0400

    Added explicit package name to test class
    Fixes test cases that were not running due to application id suffix. Fixes #89

--- app/src/test/java/ca/josephroque/bowlingcompanion/theme/ThemeTest.java
+++ app/src/test/java/ca/josephroque/bowlingcompanion/theme/ThemeTest.java
@@ -23,7 +23,7 @@ import static org.junit.Assert.assertThat;
  */
 
 @RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21)
+@Config(constants = BuildConfig.class, sdk = 21, packageName = "ca.josephroque.bowlingcompanion")
 public class ThemeTest {
 
     @Before
