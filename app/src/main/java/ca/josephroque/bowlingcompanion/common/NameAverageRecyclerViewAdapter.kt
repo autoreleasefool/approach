e47f3b63de4822f870f714e5aab471db68d46010
Joseph Roque <contact@josephroque.ca>
2018-03-10 00:56:04 -0500

    Update build dependencies

--- app/src/main/java/ca/josephroque/bowlingcompanion/common/NameAverageRecyclerViewAdapter.kt
+++ app/src/main/java/ca/josephroque/bowlingcompanion/common/NameAverageRecyclerViewAdapter.kt
@@ -61,14 +61,14 @@ class NameAverageRecyclerViewAdapter(
 
     private var itemTouchHelper: ItemTouchHelper? = null
 
-    override fun onAttachedToRecyclerView(recyclerView: RecyclerView?) {
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
         super.onAttachedToRecyclerView(recyclerView)
         this.recyclerView = recyclerView
         this.itemTouchHelper = ItemTouchHelper(SwipeCallback())
         this.itemTouchHelper?.attachToRecyclerView(this.recyclerView)
     }
 
-    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView?) {
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
         super.onDetachedFromRecyclerView(recyclerView)
         listener = null
         this.recyclerView = null
