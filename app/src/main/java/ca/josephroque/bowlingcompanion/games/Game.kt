25cbd5e66814be619bf26365b101842bb0328465
Joseph Roque <contact@josephroque.ca>
2018-04-18 06:20:43 -0400

    Enum for MatchPlayResults

--- app/src/main/java/ca/josephroque/bowlingcompanion/games/Game.kt
+++ app/src/main/java/ca/josephroque/bowlingcompanion/games/Game.kt
@@ -3,6 +3,16 @@ package ca.josephroque.bowlingcompanion.games
 /**
  * Copyright (C) 2018 Joseph Roque
  */
+
+enum class MatchPlayResult {
+    NONE, WON, LOST, TIED;
+
+    companion object {
+        private val map = MatchPlayResult.values().associateBy(MatchPlayResult::ordinal)
+        fun fromInt(type: Int) = map[type]
+    }
+}
+
 data class Game(val score: Int) {
 
     companion object {
