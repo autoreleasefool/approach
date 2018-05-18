44121b577c0097a3d9cbdc98f77e08920d4db53f
Joseph Roque <contact@josephroque.ca>
2018-05-18 09:49:53 -0400

    Frames for games

---
+++ app/src/main/java/ca/josephroque/bowlingcompanion/games/Frame.kt
@@ -0,0 +1,18 @@
+package ca.josephroque.bowlingcompanion.games
+
+/**
+ * Copyright (C) 2018 Joseph Roque
+ *
+ * A single frame in a game.
+ */
+data class Frame(val number: Int) {
+
+    companion object {
+        /** Logging identifier. */
+        @Suppress("unused")
+        private const val TAG = "Frame"
+
+        /** Number of balls in a frame. */
+        const val NUMBER_OF_BALLS = 3
+    }
+}
