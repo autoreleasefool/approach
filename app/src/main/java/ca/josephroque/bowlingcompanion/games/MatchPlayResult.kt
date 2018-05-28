46137cbf803645daefbe4e460aeb3ecb51c16b6e
Joseph Roque <contact@josephroque.ca>
2018-05-28 08:55:56 -0400

    Associate MatchPlayResult with icon

--- app/src/main/java/ca/josephroque/bowlingcompanion/games/MatchPlayResult.kt
+++ app/src/main/java/ca/josephroque/bowlingcompanion/games/MatchPlayResult.kt
@@ -1,5 +1,7 @@
 package ca.josephroque.bowlingcompanion.games
 
+import ca.josephroque.bowlingcompanion.R
+
 /**
  * Copyright (C) 2018 Joseph Roque
  *
@@ -11,5 +13,19 @@ enum class MatchPlayResult {
     companion object {
         private val map = MatchPlayResult.values().associateBy(MatchPlayResult::ordinal)
         fun fromInt(type: Int) = map[type]
+    }
+
+    /**
+     * Get an icon which represents the match play result.
+     *
+     * @return id of the icon drawable
+     */
+    fun getIcon(): Int {
+        return when (this) {
+            NONE -> R.drawable.ic_match_play_none
+            WON -> R.drawable.ic_match_play_won
+            LOST -> R.drawable.ic_match_play_lost
+            TIED -> R.drawable.ic_match_play_tied
+        }
     }
 }
