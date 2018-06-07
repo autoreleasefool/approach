daa14a910d4051745f5482c02e92968e8f1ff606
Joseph Roque <contact@josephroque.ca>
2018-06-07 15:38:31 -0700

    Fix tab bar background color

--- app/src/main/java/ca/josephroque/bowlingcompanion/NavigationActivity.kt
+++ app/src/main/java/ca/josephroque/bowlingcompanion/NavigationActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.support.annotation.IdRes
 import android.support.design.widget.BottomSheetDialogFragment
 import android.support.design.widget.FloatingActionButton
-import android.support.v4.app.DialogFragment
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentTransaction
 import android.support.v4.view.GravityCompat
