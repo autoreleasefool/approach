c69edd1ef62f2c7cc7591df3e465e7c58610e928
Joseph Roque <contact@josephroque.ca>
2018-07-11 06:47:22 -0400

    Method to refresh ball text views

--- app/src/main/java/ca/josephroque/bowlingcompanion/games/Game.kt
+++ app/src/main/java/ca/josephroque/bowlingcompanion/games/Game.kt
@@ -81,6 +81,90 @@ data class Game(
         }
 
     /**
+     * Gets the text for each ball of each frame from this game.
+     *
+     * @return the text for each ball, as a list of arrays
+     */
+    fun getBallTextForFrames(): List<Array<String>> {
+        return frames.mapIndexed { index, frame ->
+            if (!frame.isAccessed) {
+                return@mapIndexed arrayOf("", "", "")
+            }
+
+            val balls = Array(Frame.NUMBER_OF_BALLS, { "" })
+            if (frame.zeroBasedOrdinal == Game.LAST_FRAME) {
+                if (frame.pinState[0].arePinsCleared()) {
+                    // If the first ball is a strike, the next two could be strikes/spares
+                    balls[0] = Ball.Strike.toString()
+                    if (frame.pinState[1].arePinsCleared()) {
+                        // If the second ball is a strike
+                        balls[1] = Ball.Strike.toString()
+                        balls[2] = frame.pinState[2].ballValue(2, true, false)
+                    } else {
+                        // Second ball is not a strike
+                        balls[1] = frame.pinState[1].ballValue(1, false, false)
+                        balls[2] = if (frame.pinState[2].arePinsCleared())
+                             Ball.Spare.toString()
+                        else
+                            frame.pinState[2].ballValueDifference(frame.pinState[1], 2, false, false)
+                    }
+                } else {
+                    // If the first ball is not a strike, check if the second ball is a spare or not and get value
+                    balls[0] = frame.pinState[0].ballValue(0, false, false)
+                    if (frame.pinState[1].arePinsCleared()) {
+                        balls[1] = Ball.Spare.toString()
+                        balls[2] = frame.pinState[2].ballValue(2, true, true)
+                    } else {
+                        balls[1] = frame.pinState[1].ballValueDifference(frame.pinState[0], 1, false, false)
+                        balls[2] = frame.pinState[2].ballValueDifference(frame.pinState[1], 2, false, false)
+                    }
+                }
+            } else {
+                val nextFrame = frames[index + 1]
+                balls[0] = frame.pinState[0].ballValue(0, false, false)
+                if (!frame.pinState[0].arePinsCleared()) {
+                    // When the first ball is not a strike, the second and third ball values need to be calculated
+                    if (frame.pinState[1].arePinsCleared()) {
+                        balls[1] = Ball.Spare.toString()
+                        balls[2] = if (nextFrame.isAccessed)
+                            nextFrame.pinState[0].ballValue(0, false, true)
+                        else
+                            Ball.None.toString()
+                    } else {
+                        balls[1] = frame.pinState[1].ballValueDifference(frame.pinState[0], 1, false, false)
+                        balls[2] = frame.pinState[2].ballValueDifference(frame.pinState[1], 2, false, false)
+                    }
+                } else {
+                    // When the first ball is a strike, show the pins knocked down in the next frame, or empty frames
+                    if (nextFrame.isAccessed) {
+                        balls[1] = nextFrame.pinState[0].ballValue(0, false, true)
+                        if (nextFrame.pinState[0].arePinsCleared()) {
+                            // When the next frame is a strike, the 3rd ball will have to come from the frame after
+                            if (frame.zeroBasedOrdinal < Game.LAST_FRAME - 1) {
+                                val nextNextFrame = frames[index + 2]
+                                balls[2] = if (nextNextFrame.isAccessed)
+                                    nextNextFrame.pinState[0].ballValue(0, false, true)
+                                else
+                                    Ball.None.toString()
+                            } else {
+                                // In the 9th frame, the 3rd ball comes from the 10th frame's second ball
+                                balls[2] = nextFrame.pinState[1].ballValue(1, false, true)
+                            }
+                        } else {
+                            balls[2] = nextFrame.pinState[1].ballValueDifference(nextFrame.pinState[0], 1, false, true)
+                        }
+                    } else {
+                        balls[1] = Ball.None.toString()
+                        balls[2] = Ball.None.toString()
+                    }
+                }
+            }
+
+            return@mapIndexed balls
+        }
+    }
+
+    /**
      * Gets the cumulative score of each frame of this game.
      *
      * @return the cumulative scores of each frame, as strings
@@ -122,10 +206,10 @@ data class Game(
                                 if (double) {
                                     frameScores[frameIdx] += nextFrame.pinState[1].value(false)
                                 } else {
-                                    frameScores[frameIdx] += nextFrame.pinState[1].difference(nextFrame.pinState[0])
+                                    frameScores[frameIdx] += nextFrame.pinState[1].valueDifference(nextFrame.pinState[0])
                                 }
                             } else if (!double) {
-                                frameScores[frameIdx] += nextFrame.pinState[1].difference(nextFrame.pinState[0])
+                                frameScores[frameIdx] += nextFrame.pinState[1].valueDifference(nextFrame.pinState[0])
                             } else {
                                 frameScores[frameIdx] += frames[frameIdx + 2].pinState[0].value(false)
                             }
--- app/src/main/java/ca/josephroque/bowlingcompanion/games/lane/Deck.kt
+++ app/src/main/java/ca/josephroque/bowlingcompanion/games/lane/Deck.kt
@@ -1,5 +1,7 @@
 package ca.josephroque.bowlingcompanion.games.lane
 
+import ca.josephroque.bowlingcompanion.games.Game
+
 /**
  * Copyright (C) 2018 Joseph Roque
  *
@@ -45,6 +47,55 @@ fun Deck.value(onDeck: Boolean): Int {
 }
 
 /**
+ * Gets the ball value of the pins knocked down.
+ *
+ * @param ballIdx the ball to get the value of
+ * @param returnSymbol indicates if a symbol should be returned instead of the numeric value
+ * @param afterStrike indicates if the ball being counted was after a strike
+ * @return the ball value
+ */
+fun Deck.ballValue(ballIdx: Int, returnSymbol: Boolean, afterStrike: Boolean): String {
+    val value = this.value(false)
+    return when (value) {
+        0 -> Ball.None.toString()
+        2, 3, 4, 6, 9, 12 -> value.toString()
+        5 -> return if ((ballIdx == 0 || returnSymbol) && this[2].isDown) Ball.HeadPin.toString() else "5"
+        7 -> return if ((ballIdx == 0 || returnSymbol) && this[2].isDown) Ball.HeadPin2.toString() else "7"
+        8 -> return if ((ballIdx == 0 || returnSymbol) && this[2].isDown) Ball.Split.toString() else "8"
+        10 -> return if ((ballIdx == 0 || returnSymbol) && this[2].isDown && ((this[0].isDown && this[1].isDown) || (this[3].isDown && this[4].isDown))) Ball.ChopOff.toString() else "10"
+        11 -> return if ((ballIdx == 0 || returnSymbol) && this[2].isDown) Ball.Ace.toString() else "11"
+        13 -> return when {
+            (ballIdx == 0 || returnSymbol) && !this[0].isDown -> Ball.Left.toString()
+            (ballIdx == 0 || returnSymbol) && !this[4].isDown -> Ball.Right.toString()
+            else -> "13"
+        }
+        15 -> return when {
+            ballIdx == 0 || returnSymbol -> Ball.Strike.toString()
+            ballIdx == 1 && !afterStrike -> Ball.Spare.toString()
+            else -> "15"
+        }
+        else -> throw IllegalStateException("Invalid value for ball: $value")
+    }
+}
+
+/**
+ * Gets the ball value of the pins which were knocked down in the deck, but not in `other`.
+ *
+ * @param other the other deck (i.e. a previous ball)
+ * @param ballIdx the ball to get the value of
+ * @param returnSymbol indicates if a symbol should be returned instead of the numeric value
+ * @param afterStrike indicates if the ball being counted was after a strike
+ */
+fun Deck.ballValueDifference(other: Deck, ballIdx: Int, returnSymbol: Boolean, afterStrike: Boolean): String {
+    val deck = this.copyOf()
+    for (i in 0 until other.size) {
+        if (other[i].isDown) { deck[i].isDown = false }
+    }
+
+    return deck.ballValue(ballIdx, returnSymbol, afterStrike)
+}
+
+/**
  * Checks if all pins have been knocked down.
  *
  * @return true if all pins are down, false if at least one is not down.
@@ -59,6 +110,6 @@ fun Deck.arePinsCleared(): Boolean {
  * @param other the other deck (i.e. a previous ball)
  * @return the value of the difference in pins knocked down
  */
-fun Deck.difference(other: Deck): Int {
+fun Deck.valueDifference(other: Deck): Int {
     return this.filterIndexed { index, pin -> pin.isDown && !other[index].isDown }.sumBy { it.value }
 }
