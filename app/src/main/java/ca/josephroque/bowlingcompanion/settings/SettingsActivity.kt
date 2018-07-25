41a6a971e6da7dbfcb4d49b2f676b4bc520cc277
Joseph Roque <contact@josephroque.ca>
2018-07-25 16:03:24 -0400

    Add toolbar to settings for back navigation

--- app/src/main/java/ca/josephroque/bowlingcompanion/settings/SettingsActivity.kt
+++ app/src/main/java/ca/josephroque/bowlingcompanion/settings/SettingsActivity.kt
@@ -2,7 +2,9 @@ package ca.josephroque.bowlingcompanion.settings
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import ca.josephroque.bowlingcompanion.R
+import kotlinx.android.synthetic.main.activity_navigation.toolbar as toolbar
 
 /**
  * Base activity to change user settings.
@@ -14,6 +16,7 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setTheme(R.style.SettingTheme)
         setContentView(R.layout.activity_settings)
+        setupToolbar()
 
         if (savedInstanceState == null) {
             val preferenceFragment = SettingsFragment.newInstance()
@@ -21,5 +24,25 @@ class SettingsActivity : AppCompatActivity() {
             ft.add(R.id.pref_container, preferenceFragment)
             ft.commit()
         }
+    }
+
+    /** @Override */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * Set up the toolbar for title and back navigation.
+     */
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setTitle(R.string.title_activity_settings)
     }
 }
