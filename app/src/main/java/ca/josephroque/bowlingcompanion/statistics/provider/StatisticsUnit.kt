b7f9fa340043cbb82d815250106c69dfda382f53
Joseph Roque <contact@josephroque.ca>
2018-08-17 07:39:26 -0400

    Add ability to filter statistics shown per unit

--- app/src/main/java/ca/josephroque/bowlingcompanion/statistics/provider/StatisticsUnit.kt
+++ app/src/main/java/ca/josephroque/bowlingcompanion/statistics/provider/StatisticsUnit.kt
@@ -16,6 +16,7 @@ import ca.josephroque.bowlingcompanion.teams.Team
  * A single unit which provides a list of statistics to be displayed.
  */
 class StatisticsUnit(
+    private val levelOfDetail: LevelOfDetail,
     val name: String,
     val statistics: List<Statistic>
 ) : KParcelable {
@@ -48,12 +49,49 @@ class StatisticsUnit(
         fun buildFromGame(game: Game): List<StatisticsUnit> {
             return ArrayList()
         }
+
+        /** Level of statistic specificity which the unit will show. */
+        enum class LevelOfDetail {
+            Team,
+            Bowler,
+            League,
+            Series,
+            Game;
+
+            /** Categories to exclude when display this unit's statistics. */
+            val excludedCategories: Set<StatisticsCategory>
+                get() {
+                    return when (this) {
+                        Team, Bowler, League -> emptySet()
+                        Series -> setOf(StatisticsCategory.Average)
+                        Game -> setOf(StatisticsCategory.Average, StatisticsCategory.MatchPlay, StatisticsCategory.Overall)
+                    }
+                }
+
+            /** Statistics to exclude when display this unit's statistics. */
+            val exludedStatistics: Set<Statistic.Companion.Identifier>
+                get() {
+                    return when (this) {
+                        Team -> setOf(Statistic.Companion.Identifier.Bowler, Statistic.Companion.Identifier.League, Statistic.Companion.Identifier.Series, Statistic.Companion.Identifier.Game)
+                        Bowler -> setOf(Statistic.Companion.Identifier.League, Statistic.Companion.Identifier.Series, Statistic.Companion.Identifier.Game)
+                        League -> setOf(Statistic.Companion.Identifier.Series, Statistic.Companion.Identifier.Game)
+                        Series -> setOf(Statistic.Companion.Identifier.Game)
+                        Game -> setOf(Statistic.Companion.Identifier.AveragePinsLeft)
+                    }
+                }
+
+            companion object {
+                private val map = LevelOfDetail.values().associateBy(LevelOfDetail::ordinal)
+                fun fromInt(type: Int) = map[type]
+            }
+        }
     }
 
     /**
      * Construct a [StatisticsUnit] from a [Parcel].
      */
     constructor(p: Parcel): this(
+            levelOfDetail = LevelOfDetail.fromInt(p.readInt())!!,
             name = p.readString(),
             statistics = arrayListOf<Statistic>().apply {
                 val parcelableArray = p.readParcelableArray(Statistic::class.java.classLoader)
@@ -65,6 +103,8 @@ class StatisticsUnit(
 
     /** @Override */
     override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeInt(levelOfDetail.ordinal)
+        writeString(name)
         writeParcelableArray(statistics.toTypedArray(), 0)
     }
 }
