4bb163466630addf84026fa1cfda8ab09a8a4a56
Joseph Roque <contact@josephroque.ca>
2018-08-18 11:20:04 -0400

    Initial statistics implemented

---
+++ app/src/main/java/ca/josephroque/bowlingcompanion/statistics/impl/AveragePinsLeftStatistic.kt
@@ -0,0 +1,75 @@
+package ca.josephroque.bowlingcompanion.statistics.impl
+
+import android.os.Parcel
+import android.os.Parcelable
+import ca.josephroque.bowlingcompanion.R
+import ca.josephroque.bowlingcompanion.common.interfaces.parcelableCreator
+import ca.josephroque.bowlingcompanion.games.Frame
+import ca.josephroque.bowlingcompanion.games.Game
+import ca.josephroque.bowlingcompanion.statistics.AverageStatistic
+import ca.josephroque.bowlingcompanion.statistics.Statistic
+import ca.josephroque.bowlingcompanion.statistics.StatisticsCategory
+import ca.josephroque.bowlingcompanion.statistics.provider.StatisticsUnit
+
+/**
+ * Copyright (C) 2018 Joseph Roque
+ *
+ * Average pins left on deck per game.
+ */
+class AveragePinsLeftStatistic(override var total: Int, override var divisor: Int) : AverageStatistic {
+
+    // MARK: Modifiers
+
+    /** @Override */
+    override fun modify(frame: Frame) {
+        total += frame.pinsLeftOnDeck
+    }
+
+    /** @Override */
+    override fun modify(game: Game) {
+        divisor++
+    }
+
+    override val titleId = Id
+    override val id = Id.toLong()
+    override val referenceFrame = Statistic.Companion.ReferenceFrame.ByFrame
+    override val category = StatisticsCategory.PinsOnDeck
+
+    // MARK: Parcelable
+
+    companion object {
+        /** Creator, required by [Parcelable]. */
+        @Suppress("unused")
+        @JvmField val CREATOR = parcelableCreator(::AveragePinsLeftStatistic)
+
+        /** Unique ID for the statistic. */
+        const val Id = R.string.statistic_average_pins_left
+    }
+
+    /**
+     * Construct this statistic from a [Parcel].
+     */
+    constructor(p: Parcel): this(total = p.readInt(), divisor = p.readInt())
+
+    /** @Override */
+    override fun describeContents(): Int {
+        return titleId
+    }
+
+    /** @Override */
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeInt(total)
+        writeInt(divisor)
+    }
+
+    // MARK: Overrides
+
+    /** @Override */
+    override fun isModifiedBy(frame: Frame) = true
+
+    /** @Override */
+    override fun isModifiedBy(game: Game) = true
+
+    /** @Override */
+    override fun isModifiedBy(unit: StatisticsUnit) = false
+}
---
+++ app/src/main/java/ca/josephroque/bowlingcompanion/statistics/impl/GameNameStatistic.kt
@@ -0,0 +1,69 @@
+package ca.josephroque.bowlingcompanion.statistics.impl
+
+import android.os.Parcel
+import android.os.Parcelable
+import ca.josephroque.bowlingcompanion.R
+import ca.josephroque.bowlingcompanion.common.interfaces.parcelableCreator
+import ca.josephroque.bowlingcompanion.games.Frame
+import ca.josephroque.bowlingcompanion.games.Game
+import ca.josephroque.bowlingcompanion.statistics.Statistic
+import ca.josephroque.bowlingcompanion.statistics.StatisticsCategory
+import ca.josephroque.bowlingcompanion.statistics.StringStatistic
+import ca.josephroque.bowlingcompanion.statistics.provider.StatisticsUnit
+
+/**
+ * Copyright (C) 2018 Joseph Roque
+ *
+ * Name of the game whose statistics are on display.
+ */
+class GameNameStatistic(override var value: String = "") : StringStatistic {
+
+    // MARK: Modifiers
+
+    /** @Override */
+    override fun modify(unit: StatisticsUnit) {
+        value = unit.name
+    }
+
+    override val titleId = Id
+    override val id = Id.toLong()
+    override val referenceFrame = Statistic.Companion.ReferenceFrame.ByUnit
+    override val category = StatisticsCategory.General
+
+    // MARK: Parcelable
+
+    companion object {
+        /** Creator, required by [Parcelable]. */
+        @Suppress("unused")
+        @JvmField val CREATOR = parcelableCreator(::GameNameStatistic)
+
+        /** Unique ID for the statistic. */
+        const val Id = R.string.statistic_game
+    }
+
+    /**
+     * Construct this statistic from a [Parcel].
+     */
+    constructor(p: Parcel): this(value = p.readString())
+
+    /** @Override */
+    override fun describeContents(): Int {
+        return titleId
+    }
+
+    /** @Override */
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeString(value)
+    }
+
+    // MARK: Overrides
+
+    /** @Override */
+    override fun isModifiedBy(frame: Frame) = false
+
+    /** @Override */
+    override fun isModifiedBy(game: Game) = false
+
+    /** @Override */
+    override fun isModifiedBy(unit: StatisticsUnit) = true
+}
