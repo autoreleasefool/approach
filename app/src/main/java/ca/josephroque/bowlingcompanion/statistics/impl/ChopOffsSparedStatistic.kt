404128118cad45aa0952da14489e118e487a1977
Joseph Roque <contact@josephroque.ca>
2018-08-21 17:47:42 -0400

    Add spared statistics

---
+++ app/src/main/java/ca/josephroque/bowlingcompanion/statistics/impl/ChopOffsSparedStatistic.kt
@@ -0,0 +1,42 @@
+package ca.josephroque.bowlingcompanion.statistics.impl
+
+import android.os.Parcel
+import android.os.Parcelable
+import ca.josephroque.bowlingcompanion.R
+import ca.josephroque.bowlingcompanion.common.interfaces.parcelableCreator
+import ca.josephroque.bowlingcompanion.games.lane.*
+
+/**
+ * Copyright (C) 2018 Joseph Roque
+ *
+ * Percentage of possible chop offs which the user successfully spared.
+ */
+class ChopOffsSparedStatistic(numerator: Int, denominator: Int) : SecondBallStatistic(numerator, denominator) {
+
+    // MARK: Modifiers
+
+    /** @Override */
+    override fun isModifiedByFirstBall(deck: Deck) = deck.isChopOff
+
+    /** @Override */
+    override fun isModifiedBySecondBall(deck: Deck) = deck.arePinsCleared()
+
+    override val titleId = Id
+    override val id = Id.toLong()
+
+    // MARK: Parcelable
+
+    companion object {
+        /** Creator, required by [Parcelable]. */
+        @Suppress("unused")
+        @JvmField val CREATOR = parcelableCreator(::ChopOffsSparedStatistic)
+
+        /** Unique ID for the statistic. */
+        const val Id = R.string.statistic_chops_spared
+    }
+
+    /**
+     * Construct this statistic from a [Parcel].
+     */
+    constructor(p: Parcel): this(numerator = p.readInt(), denominator = p.readInt())
+}
