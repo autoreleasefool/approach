2432079fdf761bcd507ad35882227fb09f387a71
Joseph Roque <contact@josephroque.ca>
2018-08-25 22:37:36 -0400

    Remove super properties, add lots of events to track

--- app/src/main/java/ca/josephroque/bowlingcompanion/utils/Analytics.kt
+++ app/src/main/java/ca/josephroque/bowlingcompanion/utils/Analytics.kt
@@ -3,18 +3,10 @@ package ca.josephroque.bowlingcompanion.utils
 import android.content.Context
 import ca.josephroque.bowlingcompanion.BuildConfig
 import ca.josephroque.bowlingcompanion.R
-import ca.josephroque.bowlingcompanion.database.Contract.BowlerEntry
-import ca.josephroque.bowlingcompanion.database.Contract.FrameEntry
-import ca.josephroque.bowlingcompanion.database.Contract.GameEntry
-import ca.josephroque.bowlingcompanion.database.Contract.LeagueEntry
-import ca.josephroque.bowlingcompanion.database.Contract.MatchPlayEntry
-import ca.josephroque.bowlingcompanion.database.Contract.SeriesEntry
-import ca.josephroque.bowlingcompanion.database.Contract.TeamEntry
-import ca.josephroque.bowlingcompanion.database.DatabaseHelper
+import ca.josephroque.bowlingcompanion.bowlers.Bowler
+import ca.josephroque.bowlingcompanion.leagues.League
+import ca.josephroque.bowlingcompanion.series.Series
 import com.mixpanel.android.mpmetrics.MixpanelAPI
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.launch
-import java.io.File
 
 /**
  * Copyright (C) 2018 Joseph Roque
@@ -37,7 +29,6 @@ class Analytics private constructor() {
             val initInstance = dangerousInstance
             val projectToken = context.resources.getString(R.string.mixpanelToken)
             initInstance.mixpanel = MixpanelAPI.getInstance(context, projectToken)
-            initInstance.refreshSuperProperties(context)
             initInstance.initialized = true
         }
 
@@ -73,41 +64,230 @@ class Analytics private constructor() {
     /** Instance of Mixpanel to record events. */
     private lateinit var mixpanel: MixpanelAPI
 
-    /**
-     * Set user super properties.
-     */
-    fun refreshSuperProperties(context: Context) {
-        launch (CommonPool) {
-            val properties: MutableMap<String, Any> = HashMap()
+    // MARK: Team events
 
-            // Get database properties
-            val db = DatabaseHelper.getInstance(context).readableDatabase
-            val dbSize = File(db.path).length()
-            properties["Database size"] = dbSize
+    fun trackSelectTeam() {
+        if (disableTracking) return
+        mixpanel.track("Teams - Selected")
+    }
 
-            // Get row counts for each table
-            val tables = hashMapOf(
-                TeamEntry.TABLE_NAME to "Team",
-                BowlerEntry.TABLE_NAME to "Bowler",
-                LeagueEntry.TABLE_NAME to "League",
-                SeriesEntry.TABLE_NAME to "Series",
-                GameEntry.TABLE_NAME to "Game",
-                FrameEntry.TABLE_NAME to "Frame",
-                MatchPlayEntry.TABLE_NAME to "MatchPlay"
-            )
+    fun trackCreateTeam(numberOfMembers: Int) {
+        if (disableTracking) return
+        mixpanel.track("Teams - Create")
+    }
 
-            for (table in tables) {
-                val tableName = table.key
-                val property = table.value
+    fun trackDeleteTeam() {
+        if (disableTracking) return
+        mixpanel.track("Teams - Deleted")
+    }
 
-                val cursor = db.rawQuery("SELECT COALESCE(MAX(id)+1, 0) FROM $tableName", null, null)
-                val count = cursor.getInt(0)
-                properties["$property row count"] = count
-                cursor.close()
-            }
+    fun trackEditTeam() {
+        if (disableTracking) return
+        mixpanel.track("Teams - Edited")
+    }
 
-            mixpanel.registerSuperPropertiesMap(properties)
-        }
+    fun trackRorderTeamMembers() {
+        if (disableTracking) return
+        mixpanel.track("Teams - Reorder")
+    }
+
+    // MARK: Bowler events
+
+    fun trackSelectBowler() {
+        if (disableTracking) return
+        mixpanel.track("Bowlers - Select")
+    }
+
+    fun trackCreateBowler() {
+        if (disableTracking) return
+        mixpanel.track("Bowlers - Create")
+    }
+
+    fun trackDeleteBowler() {
+        if (disableTracking) return
+        mixpanel.track("Bowlers - Delete")
+    }
+
+    fun trackEditBowler() {
+        if (disableTracking) return
+        mixpanel.track("Bowlers - Edit")
+    }
+
+    fun trackSortedBowlers(order: Bowler.Companion.Sort) {
+        if (disableTracking) return
+        mixpanel.track("Bowlers - Sorted")
+    }
+
+    // MARK: League events
+
+    fun trackSelectLeague(isPractice: Boolean, isEvent: Boolean) {
+        if (disableTracking) return
+        mixpanel.track("Leagues - Select")
+    }
+
+    fun trackCreateLeague(isEvent: Boolean, numberOfGames: Int, hasAdditionalInfo: Boolean) {
+        if (disableTracking) return
+        mixpanel.track("Leagues - Create")
+    }
+
+    fun trackDeleteLeague() {
+        if (disableTracking) return
+        mixpanel.track("Leagues - Delete")
+    }
+
+    fun trackEditLeague() {
+        if (disableTracking) return
+        mixpanel.track("Leagues - Edit")
+    }
+
+    fun trackSortedLeagues(order: League.Companion.Sort) {
+        if (disableTracking) return
+        mixpanel.track("Leagues - Sorted")
+    }
+
+    // MARK: Series events
+
+    fun trackSelectSeries() {
+        if (disableTracking) return
+        mixpanel.track("Series - Select")
+    }
+
+    fun trackCreateSeries() {
+        if (disableTracking) return
+        mixpanel.track("Series - Create")
+    }
+
+    fun trackDeleteSeries() {
+        if (disableTracking) return
+        mixpanel.track("Series - Delete")
+    }
+
+    fun trackEditSeries() {
+        if (disableTracking) return
+        mixpanel.track("Series - Edit")
+    }
+
+    fun trackToggledSeriesView(view: Series.Companion.View) {
+        if (disableTracking) return
+        mixpanel.track("Series - Toggled View")
+    }
+
+    // MARK: Game events
+
+    fun trackChangedGame() {
+        if (disableTracking) return
+        mixpanel.track("Game - Select")
+    }
+
+    fun trackSetGameManualScore() {
+        if (disableTracking) return
+        mixpanel.track("Games - Manual Score")
+    }
+
+    fun trackLockGame() {
+        if (disableTracking) return
+        mixpanel.track("Games - Locked")
+    }
+
+    fun trackGameViewedPossibleScore(possibleScore: Int, frame: Int) {
+        if (disableTracking) return
+        mixpanel.track("Games - View Possible Score")
+    }
+
+    fun trackResetGame() {
+        if (disableTracking) return
+        mixpanel.track("Games - Reset")
+    }
+
+    // MARK: Match Play events
+
+    fun trackRecordMatchPlay() {
+        if (disableTracking) return
+        mixpanel.track("Match Play - Record")
+    }
+
+    // MARK: Statistics events
+
+    fun trackViewStatisticsList() {
+        if (disableTracking) return
+        mixpanel.track("Statistics - View List")
+    }
+
+    fun beginTrackingStatisticsLoaded() {
+        if (disableTracking) return
+        mixpanel.timeEvent("Statistics - Load")
+    }
+
+    fun trackStatisticsLoaded() {
+        if (disableTracking) return
+        mixpanel.track("Statistics - Load")
+    }
+
+    fun trackViewStatisticsGraph(statisticName: String) {
+        if (disableTracking) return
+        mixpanel.track("Statistics - View Graph")
+    }
+
+    // MARK: Settings
+
+    fun trackViewSettings() {
+        if (disableTracking) return
+        mixpanel.track("Settings - View")
+    }
+
+    fun trackEnableAutoAdvance() {
+        if (disableTracking) return
+        mixpanel.track("Auto Advance - Enable")
+    }
+
+    fun trackDisableAutoAdvance() {
+        if (disableTracking) return
+        mixpanel.track("Auto Advance - Disable")
+    }
+
+    fun trackEnableAutoLock() {
+        if (disableTracking) return
+        mixpanel.track("Auto Lock - Enable")
+    }
+
+    fun trackDisableAutoLock() {
+        if (disableTracking) return
+        mixpanel.track("Auto Lock - Disable")
+    }
+
+    fun trackRate() {
+        if (disableTracking) return
+        mixpanel.track("Settings - Rate")
+    }
+
+    fun trackReportBug() {
+        if (disableTracking) return
+        mixpanel.track("Settings - Report Bug")
+    }
+
+    fun trackSendFeedback() {
+        if (disableTracking) return
+        mixpanel.track("Settings - Send Feedback")
+    }
+
+    fun trackViewFacebook() {
+        if (disableTracking) return
+        mixpanel.track("Settings - View Facebook")
+    }
+
+    fun trackViewWebsite() {
+        if (disableTracking) return
+        mixpanel.track("Settings - View Website")
+    }
+
+    fun trackViewSource() {
+        if (disableTracking) return
+        mixpanel.track("Settings - View Source")
+    }
+
+    fun trackViewAttributions() {
+        if (disableTracking) return
+        mixpanel.track("Settings - View Attributions")
     }
 
     /**
