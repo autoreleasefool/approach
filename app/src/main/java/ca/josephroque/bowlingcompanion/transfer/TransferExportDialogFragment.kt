8e732ebf9c9ef4248341e94f507c4330212016a0
Joseph Roque <contact@josephroque.ca>
2018-09-04 18:51:20 -0400

    Layout and fragment for transfer import and export

--- app/src/main/java/ca/josephroque/bowlingcompanion/transfer/TransferExportDialogFragment.kt
+++ app/src/main/java/ca/josephroque/bowlingcompanion/transfer/TransferExportDialogFragment.kt
@@ -7,44 +7,32 @@ import android.view.View
 import android.view.ViewGroup
 import ca.josephroque.bowlingcompanion.R
 import ca.josephroque.bowlingcompanion.common.fragments.BaseDialogFragment
-import kotlinx.android.synthetic.main.dialog_transfer.view.*
+import kotlinx.android.synthetic.main.dialog_transfer_export.view.*
 
 /**
  * Copyright (C) 2018 Joseph Roque
  *
- * A fragment to enable transferring the user's data to a new device, or transferring from another device to their current one.
+ * A dialog fragment to export user's data.
  */
-class TransferDialogFragment : BaseDialogFragment() {
+class TransferExportDialogFragment : BaseDialogFragment() {
 
     companion object {
         /** Logging identifier */
         @Suppress("unused")
-        private const val TAG = "TransferDialogFragment"
+        private const val TAG = "TransferExportDialogFragment"
 
         /**
          * Create a new instance.
          *
          * @return the new instance
          */
-        fun newInstance(): TransferDialogFragment {
-            return TransferDialogFragment()
+        fun newInstance(): TransferExportDialogFragment {
+            return TransferExportDialogFragment()
         }
     }
 
     private val onClickListener = View.OnClickListener {
-        when (it.id) {
-            R.id.btn_export -> {
-                val newFragment = TransferExportDialogFragment.newInstance()
-                fragmentNavigation?.pushDialogFragment(newFragment)
-            }
-            R.id.btn_import -> {
-                val newFragment = TransferImportDialogFragment.newInstance()
-                fragmentNavigation?.pushDialogFragment(newFragment)
-            }
-            R.id.btn_restore_delete -> {
-                TODO("Show restore/delete menu")
-            }
-        }
+        TODO("Begin export")
     }
 
     /** @Override */
@@ -55,12 +43,10 @@ class TransferDialogFragment : BaseDialogFragment() {
 
     /** @Override */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.dialog_transfer, container, false)
+        val view = inflater.inflate(R.layout.dialog_transfer_export, container, false)
 
         setupToolbar(view)
         view.btn_export.setOnClickListener(onClickListener)
-        view.btn_import.setOnClickListener(onClickListener)
-        view.btn_restore_delete.setOnClickListener(onClickListener)
 
         return view
     }
@@ -72,11 +58,12 @@ class TransferDialogFragment : BaseDialogFragment() {
      */
     private fun setupToolbar(rootView: View) {
         rootView.toolbar_transfer.apply {
-            setTitle(R.string.transfer_data)
-            setNavigationIcon(R.drawable.ic_dismiss)
+            setTitle(R.string.export)
+            setNavigationIcon(R.drawable.ic_arrow_back)
             setNavigationOnClickListener {
                 dismiss()
             }
         }
     }
+
 }
