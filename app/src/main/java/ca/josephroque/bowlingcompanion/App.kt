b1739644b4aa5ee9c263b30cb8ed9ebaef96bb40
Joseph Roque <contact@josephroque.ca>
2018-09-18 10:06:03 -0400

    Adview

--- app/src/main/java/ca/josephroque/bowlingcompanion/App.kt
+++ app/src/main/java/ca/josephroque/bowlingcompanion/App.kt
@@ -8,6 +8,7 @@ import android.arch.lifecycle.OnLifecycleEvent
 import android.content.Context
 import android.support.v7.preference.PreferenceManager
 import android.view.inputmethod.InputMethodManager
+import com.google.android.gms.ads.MobileAds
 import java.util.concurrent.atomic.AtomicBoolean
 
 /**
@@ -18,10 +19,33 @@ import java.util.concurrent.atomic.AtomicBoolean
  */
 class App : Application(), LifecycleObserver {
 
-    /** @Override */
+    companion object {
+        @Suppress("unused")
+        private const val TAG = "BowlingCompanionApp"
+
+        val isRunning: AtomicBoolean = AtomicBoolean(false)
+
+        fun hideSoftKeyBoard(activity: Activity) {
+            activity.currentFocus?.let {
+                val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(it.windowToken, 0)
+            }
+        }
+
+        fun showSoftKeyBoard(activity: Activity) {
+            activity.currentFocus?.let {
+                val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.showSoftInput(it, 0)
+            }
+        }
+    }
+
+    // MARK: Lifecycle functions
+
     override fun onCreate() {
         super.onCreate()
         PreferenceManager.setDefaultValues(this, R.xml.pref_app, false)
+        MobileAds.initialize(this, BuildConfig.BANNER_AD_UNIT_ID)
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
@@ -32,38 +56,5 @@ class App : Application(), LifecycleObserver {
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun onAppForegrounded() {
         isRunning.set(true)
-    }
-
-    companion object {
-        /** Logging identifier. */
-        @Suppress("unused")
-        private const val TAG = "BowlingCompanionApp"
-
-        /** Indicates if the app is running or not. */
-        val isRunning: AtomicBoolean = AtomicBoolean(false)
-
-        /**
-         * Hides the soft keyboard with the current activity.
-         *
-         * @param activity the activity context
-         */
-        fun hideSoftKeyBoard(activity: Activity) {
-            activity.currentFocus?.let {
-                val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.hideSoftInputFromWindow(it.windowToken, 0)
-            }
-        }
-
-        /**
-         * Shows the soft keyboard with the current activity.
-         *
-         * @param activity the activity context
-         */
-        fun showSoftKeyBoard(activity: Activity) {
-            activity.currentFocus?.let {
-                val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.showSoftInput(it, 0)
-            }
-        }
     }
 }
