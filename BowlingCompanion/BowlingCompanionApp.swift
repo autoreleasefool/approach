ea6d57f565e082fc4050d08c0f1d55813276d8a5
Joseph Roque <contact@josephroque.ca>
2022-09-16 23:10:47 -0700

    Initial project commit

---
+++ BowlingCompanion/BowlingCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowlingCompanionApp.swift
+//  BowlingCompanion
+//
+//  Created by Joseph Roque on 2022-09-16.
+//
+
+import SwiftUI
+
+@main
+struct BowlingCompanionApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
