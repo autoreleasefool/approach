9dc5cb8e5c9b027c3ea320bac77e0ebc4764c95d
Joseph Roque <contact@josephroque.ca>
2022-10-16 10:12:42 -0700

    Additional tests for BowlersList state

--- BowlingCompanion/Tests/BowlersListFeatureTests/BowlersListFeatureTests.swift
+++ BowlingCompanion/Tests/BowlersListFeatureTests/BowlersListFeatureTests.swift
@@ -25,4 +25,68 @@ final class BowlersListFeatureTests: XCTestCase {
 
 		await store.send(.onDisappear).finish()
 	}
+
+	func testNavigatesToLeague() async {
+		let store = TestStore(
+			initialState: BowlersList.State(),
+			reducer: BowlersList()
+		)
+
+		let mockBowler = Bowler(id: UUID(), name: "Bowler")
+
+		await store.send(.bowlersResponse(.success([mockBowler]))) {
+			$0.bowlers = .init(uniqueElements: [mockBowler])
+		}.finish()
+
+		await store.send(.setNavigation(selection: mockBowler.id)) {
+			$0.selection = Identified(.init(bowler: mockBowler), id: mockBowler.id)
+		}.finish()
+	}
+
+	func testDismissesNavigationToLeague() async {
+		let store = TestStore(
+			initialState: BowlersList.State(),
+			reducer: BowlersList()
+		)
+
+		let mockBowler = Bowler(id: UUID(), name: "Bowler")
+
+		await store.send(.bowlersResponse(.success([mockBowler]))) {
+			$0.bowlers = .init(uniqueElements: [mockBowler])
+		}.finish()
+
+		await store.send(.setNavigation(selection: mockBowler.id)) {
+			$0.selection = Identified(.init(bowler: mockBowler), id: mockBowler.id)
+		}.finish()
+
+		await store.send(.setNavigation(selection: nil)) {
+			$0.selection = nil
+		}.finish()
+	}
+
+	func testShowsBowlerForm() async {
+		let store = TestStore(
+			initialState: BowlersList.State(),
+			reducer: BowlersList()
+		)
+
+		await store.send(.setFormSheet(isPresented: true)) {
+			$0.bowlerForm = .init(mode: .create)
+		}.finish()
+	}
+
+	func testDismissesBowlerForm() async {
+		let store = TestStore(
+			initialState: BowlersList.State(),
+			reducer: BowlersList()
+		)
+
+		await store.send(.setFormSheet(isPresented: true)) {
+			$0.bowlerForm = .init(mode: .create)
+		}.finish()
+
+		await store.send(.setFormSheet(isPresented: false)) {
+			$0.bowlerForm = nil
+		}.finish()
+	}
 }
