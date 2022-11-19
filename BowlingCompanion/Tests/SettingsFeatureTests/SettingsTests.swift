054be337733721d28ef0d978c9a2c8f3cfae86e0
Joseph Roque <joseph@atob.com>
2022-11-19 00:16:38 -0500

    Update tests for settings

--- BowlingCompanion/Tests/SettingsFeatureTests/SettingsTests.swift
+++ BowlingCompanion/Tests/SettingsFeatureTests/SettingsTests.swift
@@ -1,15 +1,20 @@
 import ComposableArchitecture
+import FeatureFlagServiceInterface
 import SettingsFeature
 import XCTest
 
 @MainActor
 final class SettingsTests: XCTestCase {
-	func testPlaceholder() async {
-		let store = TestStore(
-			initialState: Settings.State(),
-			reducer: Settings()
-		)
+	func testShowsFeatures() {
+		DependencyValues.withValues {
+			$0.featureFlags.isEnabled = { _ in true }
+		} operation: {
+			let store = TestStore(
+				initialState: Settings.State(),
+				reducer: Settings()
+			)
 
-		await store.send(.placeholder).finish()
+			XCTAssertTrue(store.state.showsFeatures)
+		}
 	}
 }
