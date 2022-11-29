4fb9918c1cb9d208a5d5b88a60b9634745e71de0
Joseph Roque <contact@josephroque.ca>
2022-11-29 13:40:05 -0800

    Colors for alley filters

--- BowlingCompanion/Sources/ThemesLibrary/Colors.swift
+++ BowlingCompanion/Sources/ThemesLibrary/Colors.swift
@@ -10,4 +10,13 @@ extension Color {
 	public static let appError = Color("Colors/Error/Default")
 	public static let appErrorLight = Color("Colors/Error/Light")
 	public static let appDestructive = Color("Colors/Destructive/Default")
+
+	public static let appAlleyMaterialBorder = Color("Colors/Alley/Material/Border")
+	public static let appAlleyMaterialBackground = Color("Colors/Alley/Material/Background")
+	public static let appAlleyMechanismBorder = Color("Colors/Alley/Mechanism/Border")
+	public static let appAlleyMechanismBackground = Color("Colors/Alley/Mechanism/Background")
+	public static let appAlleyPinBaseBorder = Color("Colors/Alley/PinBase/Border")
+	public static let appAlleyPinBaseBackground = Color("Colors/Alley/PinBase/Background")
+	public static let appAlleyPinFallBorder = Color("Colors/Alley/PinFall/Border")
+	public static let appAlleyPinFallBackground = Color("Colors/Alley/PinFall/Background")
 }
---
+++ BowlingCompanion/Sources/ViewsLibrary/BadgeView.swift
@@ -0,0 +1,62 @@
+import SwiftUI
+import ThemesLibrary
+
+public struct BadgeView: View {
+	let text: String
+	let style: Style
+
+	public init(_ text: String, style: Style = .plain) {
+		self.text = text
+		self.style = style
+	}
+
+	public var body: some View {
+		Text(text)
+			.lineLimit(1)
+			.font(.caption)
+			.foregroundColor(style.foregroundColor)
+			.padding(.vertical, .tinySpacing)
+			.padding(.horizontal, .smallSpacing)
+			.background(
+				RoundedRectangle(cornerRadius: .standardRadius)
+					.strokeBorder(style.foregroundColor, lineWidth: 1)
+					.background(
+						RoundedRectangle(cornerRadius: .standardRadius)
+							.foregroundColor(style.backgroundColor)
+					)
+			)
+	}
+}
+
+extension BadgeView {
+	public enum Style {
+		case primary
+		case plain
+		case success
+		case destructive
+		case info
+		case custom(foreground: Color, background: Color)
+
+		var foregroundColor: Color {
+			switch self {
+			case .primary: return .appPrimary
+			case .plain: return .black
+			case .success: return .green
+			case .destructive: return .appDestructive
+			case .info: return .blue
+			case let .custom(foreground, _): return foreground
+			}
+		}
+
+		var backgroundColor: Color {
+			switch self {
+			case .primary: return .appPrimaryLight
+			case .plain: return .gray
+			case .success: return .teal
+			case .destructive: return .pink
+			case .info: return .cyan
+			case let .custom(_, background): return background
+			}
+		}
+	}
+}
