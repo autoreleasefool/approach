95440e6d4a2e56dd5001b6ac51bec23c6c9dc41a
Joseph Roque <contact@josephroque.ca>
2022-12-09 18:59:25 -0800

    Add feature flag for lane tracking

--- BowlingCompanion/Sources/FeatureFlagLibrary/FeatureFlags.swift
+++ BowlingCompanion/Sources/FeatureFlagLibrary/FeatureFlags.swift
@@ -5,11 +5,13 @@ extension FeatureFlag {
 	public static let alleyTracking = Self(name: "alleyTracking", introduced: "2022-11-09", stage: .development)
 	public static let gearTracking = Self(name: "gearTracking", introduced: "2022-11-09", stage: .development)
 	public static let settingsTab = Self(name: "settingsTab", introduced: "2022-11-09", stage: .development, isOverridable: false)
+	public static let lanesTracking = Self(name: "lanesTracking", introduced: "2022-12-09", stage: .development)
 	public static let developerOptions = Self(name: "developerOptions", introduced: "2022-11-10", stage: .development, isOverridable: false)
 
 	public static let allFlags: [Self] = [
 		.alleyTracking,
 		.gearTracking,
+		.lanesTracking,
 		.scoreSheetTab,
 		.settingsTab,
 		.developerOptions,
