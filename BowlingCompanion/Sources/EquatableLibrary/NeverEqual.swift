6099f6fbdac0263a1d023e32555cf84fdbbdf91f
Joseph Roque <contact@josephroque.ca>
2023-01-16 21:12:11 -0800

    Move NeverEqual into its own file

--- BowlingCompanion/Sources/EquatableLibrary/NeverEqual.swift
+++ BowlingCompanion/Sources/EquatableLibrary/NeverEqual.swift
@@ -1,10 +1,10 @@
-public struct AlwaysEqual<V>: Equatable {
+public struct NeverEqual<V>: Equatable {
 	public let wrapped: V
 	public init(wrapped: V) {
 		self.wrapped = wrapped
 	}
 
 	public static func == (lhs: Self, rhs: Self) -> Bool {
-		true
+		false
 	}
 }
