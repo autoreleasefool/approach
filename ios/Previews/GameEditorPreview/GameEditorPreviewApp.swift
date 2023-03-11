f79e38903e1637834c0e38b94a746ca944702691
Joseph Roque <joseph@atob.com>
2023-03-11 22:26:13 +0100

    GamesEditorPreview app for quicker building

---
+++ ios/Previews/GameEditorPreview/GameEditorPreviewApp.swift
@@ -0,0 +1,51 @@
+import ComposableArchitecture
+import GamesEditorFeature
+import SharedModelsLibrary
+import SwiftUI
+
+@main
+struct GameEditorPreviewApp: App {
+	let store: Store = {
+		return .init(
+			initialState: GamesEditor.State(games: .init(uniqueElements: Game.mocks), selected: Game.mocks.first!.id),
+			reducer: GamesEditor()._printChanges()
+		)
+	}()
+
+	var body: some Scene {
+		WindowGroup {
+			NavigationView {
+				GamesEditorView(store: store)
+			}
+		}
+	}
+}
+
+extension Game {
+	public static let mocks: [Self] = [
+		.init(
+			series: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
+			id: UUID(uuidString: "00000000-0000-0000-0000-000000000002")!,
+			ordinal: 1,
+			locked: .unlocked,
+			manualScore: nil,
+			excludeFromStatistics: .include
+		),
+		.init(
+			series: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
+			id: UUID(uuidString: "00000000-0000-0000-0000-000000000003")!,
+			ordinal: 2,
+			locked: .unlocked,
+			manualScore: nil,
+			excludeFromStatistics: .include
+		),
+		.init(
+			series: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
+			id: UUID(uuidString: "00000000-0000-0000-0000-000000000004")!,
+			ordinal: 3,
+			locked: .unlocked,
+			manualScore: nil,
+			excludeFromStatistics: .include
+		)
+	]
+}
