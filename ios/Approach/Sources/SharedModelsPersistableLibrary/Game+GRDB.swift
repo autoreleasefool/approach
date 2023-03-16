608996d54adc94ce498a3c20719d3cbe4d48bfa2
Joseph Roque <contact@josephroque.ca>
2023-03-16 08:49:06 -0700

    Insert frames when creating game

--- ios/Approach/Sources/SharedModelsPersistableLibrary/Game+GRDB.swift
+++ ios/Approach/Sources/SharedModelsPersistableLibrary/Game+GRDB.swift
@@ -6,4 +6,18 @@ extension Game: FetchableRecord, PersistableRecord {
 	public func willSave(_ db: Database) throws {
 		guard id != .placeholder else { throw ValidationError.usingPlaceholderId }
 	}
+
+	public func aroundInsert(_ db: Database, insert: () throws -> InsertionSuccess) throws {
+		_ = try insert()
+
+		for ordinal in (1...Game.NUMBER_OF_FRAMES) {
+			let frame = Frame(
+				game: id,
+				ordinal: ordinal,
+				rolls: []
+			)
+
+			try frame.insert(db)
+		}
+	}
 }
