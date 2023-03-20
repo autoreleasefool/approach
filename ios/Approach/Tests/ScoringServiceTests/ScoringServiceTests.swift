368aee93d62dc9dc66c3094ebe39bc56e2f2d403
Joseph Roque <contact@josephroque.ca>
2023-03-20 16:16:01 -0700

    Stub tests

--- ios/Approach/Tests/ScoringServiceTests/ScoringServiceTests.swift
+++ ios/Approach/Tests/ScoringServiceTests/ScoringServiceTests.swift
@@ -4,5 +4,35 @@ import XCTest
 
 @MainActor
 final class ScoringServiceTests: XCTestCase {
+	func testCalculatesScoreForGame() {
+		// TODO: testCalculatesScoreForGame
+	}
 
+	func testCalculatesScoreForFrames() {
+		// TODO: testCalculatesScoreForFrames
+	}
+
+	func testCalculatesScoreForFramesWithSteps() {
+		// TODO: testCalculatesScoreForFramesWithSteps
+	}
+
+	func testScoresStrikeCorrectly() {
+		// TODO: testScoresStrikeCorrectly
+	}
+
+	func testScoresSpareCorrectly() {
+		// TODO: testScoresSpareCorrectly
+	}
+
+	func testScoresStrikeInLastFrameCorrectly() {
+		// TODO: testScoresStrikeInLastFrameCorrectly
+	}
+
+	func testScoresSpareInLastFrameCorrectly() {
+		// TODO: testScoresSpareInLastFrameCorrectly
+	}
+
+	func testScoresOpenFrameCorrectly() {
+		// TODO: testScoresOpenFrameCorrectly
+	}
 }
