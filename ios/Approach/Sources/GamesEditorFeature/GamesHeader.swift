ca13f6f399d4501a6371189c41e64e7988c144ef
Joseph Roque <contact@josephroque.ca>
2023-05-13 17:42:07 +0530

    Chore: Merge views into reducer files

--- ios/Approach/Sources/GamesEditorFeature/GamesHeader.swift
+++ ios/Approach/Sources/GamesEditorFeature/GamesHeader.swift
@@ -1,6 +1,10 @@
+import AssetsLibrary
 import ComposableArchitecture
 import FeatureActionLibrary
 import ModelsLibrary
+import StringsLibrary
+import SwiftUI
+import ViewsLibrary
 
 public struct GamesHeader: Reducer {
 	public struct State: Equatable {
@@ -38,6 +42,53 @@ public struct GamesHeader: Reducer {
 			case .delegate:
 				return .none
 			}
+		}
+	}
+}
+
+// MARK: - View
+
+public struct GamesHeaderView: View {
+	let store: StoreOf<GamesHeader>
+
+	enum ViewAction {
+		case didTapClose
+		case didTapSettings
+	}
+
+	init(store: StoreOf<GamesHeader>) {
+		self.store = store
+	}
+
+	public var body: some View {
+		WithViewStore(store, observe: { $0 }, send: GamesHeader.Action.init, content: { viewStore in
+			HStack {
+				headerButton(systemName: "chevron.backward") { viewStore.send(.didTapClose) }
+				Spacer()
+				headerButton(systemName: "gear") { viewStore.send(.didTapSettings) }
+			}
+		})
+	}
+
+	private func headerButton(systemName: String, action: @escaping () -> Void) -> some View {
+		Button(action: action) {
+			Image(systemName: systemName)
+				.resizable()
+				.scaledToFit()
+				.frame(width: .smallIcon, height: .smallIcon)
+				.foregroundColor(.white)
+				.padding()
+		}
+	}
+}
+
+extension GamesHeader.Action {
+	init(action: GamesHeaderView.ViewAction) {
+		switch action {
+		case .didTapClose:
+			self = .delegate(.didCloseEditor)
+		case .didTapSettings:
+			self = .delegate(.didOpenSettings)
 		}
 	}
 }
--- ios/Approach/Sources/GamesEditorFeature/GamesSettings.swift
+++ ios/Approach/Sources/GamesEditorFeature/GamesSettings.swift
@@ -2,6 +2,8 @@ import ComposableArchitecture
 import FeatureActionLibrary
 import GamesRepositoryInterface
 import ModelsLibrary
+import StringsLibrary
+import SwiftUI
 
 public struct GamesSettings: Reducer {
 	public struct State: Equatable {
@@ -42,6 +44,45 @@ public struct GamesSettings: Reducer {
 			case .delegate:
 				return .none
 			}
+		}
+	}
+}
+
+// MARK: - View
+
+public struct GamesSettingsView: View {
+	let store: StoreOf<GamesSettings>
+
+	struct ViewState: Equatable {
+		init(state: GamesSettings.State) {}
+	}
+
+	enum ViewAction {
+		case didTapDone
+	}
+
+	init(store: StoreOf<GamesSettings>) {
+		self.store = store
+	}
+
+	public var body: some View {
+		WithViewStore(store, observe: ViewState.init, send: GamesSettings.Action.init) { viewStore in
+			EmptyView()
+				.navigationTitle(Strings.Game.Settings.title)
+				.toolbar {
+					ToolbarItem(placement: .navigationBarLeading) {
+						Button(Strings.Action.done) { viewStore.send(.didTapDone) }
+					}
+				}
+		}
+	}
+}
+
+extension GamesSettings.Action {
+	init(action: GamesSettingsView.ViewAction) {
+		switch action {
+		case .didTapDone:
+			self = .delegate(.didFinish)
 		}
 	}
 }
