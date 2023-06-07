2c2085bad95ca9e7d15737647852c06d43913825
Joseph Roque <contact@josephroque.ca>
2023-06-07 12:39:07 -0700

    Fix: Remove unused ViewStore in OpponentsList

--- ios/Approach/Sources/OpponentsListFeature/OpponentsListView.swift
+++ ios/Approach/Sources/OpponentsListFeature/OpponentsListView.swift
@@ -13,31 +13,25 @@ import ViewsLibrary
 public struct OpponentsListView: View {
 	let store: StoreOf<OpponentsList>
 
-	struct ViewState: Equatable {
-		init(state: OpponentsList.State) {}
-	}
-
 	public init(store: StoreOf<OpponentsList>) {
 		self.store = store
 	}
 
 	public var body: some View {
-		WithViewStore(store, observe: ViewState.init) { viewStore in
-			ResourceListView(
-				store: store.scope(state: \.list, action: /OpponentsList.Action.InternalAction.list)
-			) { opponent in
-				Text(opponent.name)
+		ResourceListView(
+			store: store.scope(state: \.list, action: /OpponentsList.Action.InternalAction.list)
+		) { opponent in
+			Text(opponent.name)
+		}
+		.navigationTitle(Strings.Opponent.List.title)
+		.toolbar {
+			ToolbarItem(placement: .navigationBarTrailing) {
+				SortOrderView(store: store.scope(state: \.sortOrder, action: /OpponentsList.Action.InternalAction.sortOrder))
 			}
-			.navigationTitle(Strings.Opponent.List.title)
-			.toolbar {
-				ToolbarItem(placement: .navigationBarTrailing) {
-					SortOrderView(store: store.scope(state: \.sortOrder, action: /OpponentsList.Action.InternalAction.sortOrder))
-				}
-			}
-			.sheet(store: store.scope(state: \.$editor, action: { .internal(.editor($0)) })) { scopedStore in
-				NavigationStack {
-					BowlerEditorView(store: scopedStore)
-				}
+		}
+		.sheet(store: store.scope(state: \.$editor, action: { .internal(.editor($0)) })) { scopedStore in
+			NavigationStack {
+				BowlerEditorView(store: scopedStore)
 			}
 		}
 	}
