9d426293d85c35be82fce59fa0cb28001e536ab2
Joseph Roque <contact@josephroque.ca>
2023-07-18 17:26:03 -0700

    Refactor: Extract BowlerForm composable

--- android/app/src/main/java/ca/josephroque/bowlingcompanion/feature/bowlerform/BowlerFormScreen.kt
+++ android/app/src/main/java/ca/josephroque/bowlingcompanion/feature/bowlerform/BowlerFormScreen.kt
@@ -1,14 +1,8 @@
 package ca.josephroque.bowlingcompanion.feature.bowlerform
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -17,22 +11,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import ca.josephroque.bowlingcompanion.R
-import ca.josephroque.bowlingcompanion.core.model.BowlerKind
+import ca.josephroque.bowlingcompanion.feature.bowlerform.ui.BowlerForm
 
 @Composable
 internal fun BowlerFormRoute(
@@ -144,66 +132,4 @@ internal fun FloatingActionButton(
 			)
 		}
 	}
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-internal fun BowlerForm(
-	name: String,
-	errorId: Int?,
-	onNameChanged: (String) -> Unit,
-	onDoneClicked: () -> Unit,
-	modifier: Modifier = Modifier,
-) {
-	Column(
-		modifier = modifier.fillMaxSize()
-	) {
-		OutlinedTextField(
-			value = name,
-			onValueChange = onNameChanged,
-			label = { Text(stringResource(R.string.bowler_form_name)) },
-			singleLine = true,
-			isError = errorId != null,
-			supportingText = {
-				if (errorId != null) {
-					Text(
-						text = stringResource(errorId),
-						color = MaterialTheme.colorScheme.error,
-						modifier = Modifier.fillMaxWidth(),
-					)
-				}
-			},
-			trailingIcon = {
-				if (errorId != null) {
-					Icon(
-						Icons.Default.Warning,
-						tint = MaterialTheme.colorScheme.error,
-						contentDescription = null
-					)
-				}
-			},
-			keyboardOptions = KeyboardOptions(
-				imeAction = ImeAction.Done,
-			),
-			keyboardActions = KeyboardActions(
-				onDone = { onDoneClicked() },
-			),
-			modifier = Modifier
-				.fillMaxWidth()
-				.padding(horizontal = 16.dp),
-		)
-	}
-}
-
-@Preview
-@Composable
-fun BowlerFormPreview() {
-	BowlerFormScreen(
-		bowlerFormUiState = BowlerFormUiState.Create(
-			name = "Joseph",
-			kind = BowlerKind.PLAYABLE,
-			fieldErrors = BowlerFormFieldErrors(nameErrorId = R.string.bowler_form_name_missing)
-		),
-		handleEvent = {},
-	)
 }
