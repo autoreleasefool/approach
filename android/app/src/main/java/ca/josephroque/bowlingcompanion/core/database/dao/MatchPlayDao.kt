e2e7920704c5084664e69020557b4ed591651d6a
Joseph Roque <contact@josephroque.ca>
2023-08-03 18:36:42 -0700

    Feat: Migrate match plays to new data format

--- android/app/src/main/java/ca/josephroque/bowlingcompanion/core/database/dao/MatchPlayDao.kt
+++ android/app/src/main/java/ca/josephroque/bowlingcompanion/core/database/dao/MatchPlayDao.kt
@@ -1,7 +1,12 @@
 package ca.josephroque.bowlingcompanion.core.database.dao
 
 import androidx.room.Dao
+import androidx.room.Query
 import ca.josephroque.bowlingcompanion.core.database.model.MatchPlayEntity
+import java.util.UUID
 
 @Dao
-abstract class MatchPlayDao: BaseDao<MatchPlayEntity>
+abstract class MatchPlayDao: BaseDao<MatchPlayEntity> {
+	@Query("SELECT * FROM match_plays WHERE game_id IN (:gameIds)")
+	abstract suspend fun getMatchPlaysForGames(gameIds: Collection<UUID>): List<MatchPlayEntity>
+}
