78564167342e01bd4ec7b61587fd01932d5b53b9
Joseph Roque <github@josephroque.ca>
2023-08-28 17:35:38 -0700

    Feat: Release lanes feature

--- ios/Approach/Sources/FeatureFlagsLibrary/FeatureFlags.swift
+++ ios/Approach/Sources/FeatureFlagsLibrary/FeatureFlags.swift
@@ -4,7 +4,7 @@ extension FeatureFlag {
 	public static let alleys = Self(name: "alleys", introduced: "2022-11-09", stage: .release)
 	public static let gear = Self(name: "gear", introduced: "2022-11-09", stage: .release)
 	public static let settingsTab = Self(name: "settingsTab", introduced: "2022-11-09", stage: .release, isOverridable: false)
-	public static let lanes = Self(name: "lanes", introduced: "2022-12-09", stage: .development)
+	public static let lanes = Self(name: "lanes", introduced: "2022-12-09", stage: .release)
 	public static let developerOptions = Self(name: "developerOptions", introduced: "2022-11-10", stage: .development, isOverridable: false)
 	public static let teams = Self(name: "teams", introduced: "2023-01-03", stage: .development)
 	public static let opponents = Self(name: "opponents", introduced: "2023-01-08", stage: .development)
