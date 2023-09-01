382ebbd619551c3162d40fad768c451a535a2000
Joseph Roque <contact@josephroque.ca>
2023-08-31 23:10:32 -0400

    Feat: Color schemes for sharing games

--- ios/Approach/Sources/AssetsLibrary/Assets.swift
+++ ios/Approach/Sources/AssetsLibrary/Assets.swift
@@ -95,29 +95,48 @@ public enum Asset {
         public static let `default` = ColorAsset(name: "ScoreSheet/Background/Default")
         public static let highlight = ColorAsset(name: "ScoreSheet/Background/Highlight")
         public static let plain = ColorAsset(name: "ScoreSheet/Background/Plain")
+        public static let pride = ColorAsset(name: "ScoreSheet/Background/Pride")
       }
       public enum Border {
-        public static let bold = ColorAsset(name: "ScoreSheet/Border/Bold")
         public static let `default` = ColorAsset(name: "ScoreSheet/Border/Default")
-        public static let defaultBold = ColorAsset(name: "ScoreSheet/Border/DefaultBold")
+        public static let defaultStrong = ColorAsset(name: "ScoreSheet/Border/DefaultStrong")
         public static let plain = ColorAsset(name: "ScoreSheet/Border/Plain")
+        public static let plainStrong = ColorAsset(name: "ScoreSheet/Border/PlainStrong")
+        public static let pride = ColorAsset(name: "ScoreSheet/Border/Pride")
+        public static let prideStrong = ColorAsset(name: "ScoreSheet/Border/PrideStrong")
       }
-      public enum Label {
-        public static let `default` = ColorAsset(name: "ScoreSheet/Label/Default")
-        public static let highlight = ColorAsset(name: "ScoreSheet/Label/Highlight")
-        public static let plain = ColorAsset(name: "ScoreSheet/Label/Plain")
+      public enum Card {
+        public static let `default` = ColorAsset(name: "ScoreSheet/Card/Default")
+        public static let highlight = ColorAsset(name: "ScoreSheet/Card/Highlight")
+        public static let plain = ColorAsset(name: "ScoreSheet/Card/Plain")
+        public static let pride = ColorAsset(name: "ScoreSheet/Card/Pride")
       }
       public enum Rail {
         public static let `default` = ColorAsset(name: "ScoreSheet/Rail/Default")
         public static let highlight = ColorAsset(name: "ScoreSheet/Rail/Highlight")
         public static let plain = ColorAsset(name: "ScoreSheet/Rail/Plain")
+        public static let pride = ColorAsset(name: "ScoreSheet/Rail/Pride")
       }
       public enum Text {
-        public static let `default` = ColorAsset(name: "ScoreSheet/Text/Default")
-        public static let foul = ColorAsset(name: "ScoreSheet/Text/Foul")
-        public static let highlight = ColorAsset(name: "ScoreSheet/Text/Highlight")
-        public static let highlightFoul = ColorAsset(name: "ScoreSheet/Text/HighlightFoul")
-        public static let plain = ColorAsset(name: "ScoreSheet/Text/Plain")
+        public enum OnBackground {
+          public static let `default` = ColorAsset(name: "ScoreSheet/Text/OnBackground/Default")
+          public static let foul = ColorAsset(name: "ScoreSheet/Text/OnBackground/Foul")
+          public static let highlight = ColorAsset(name: "ScoreSheet/Text/OnBackground/Highlight")
+          public static let highlightFoul = ColorAsset(name: "ScoreSheet/Text/OnBackground/HighlightFoul")
+          public static let plain = ColorAsset(name: "ScoreSheet/Text/OnBackground/Plain")
+          public static let pride = ColorAsset(name: "ScoreSheet/Text/OnBackground/Pride")
+        }
+        public enum OnCard {
+          public static let `default` = ColorAsset(name: "ScoreSheet/Text/OnCard/Default")
+          public static let plain = ColorAsset(name: "ScoreSheet/Text/OnCard/Plain")
+          public static let pride = ColorAsset(name: "ScoreSheet/Text/OnCard/Pride")
+        }
+        public enum OnRail {
+          public static let `default` = ColorAsset(name: "ScoreSheet/Text/OnRail/Default")
+          public static let highlight = ColorAsset(name: "ScoreSheet/Text/OnRail/Highlight")
+          public static let plain = ColorAsset(name: "ScoreSheet/Text/OnRail/Plain")
+          public static let pride = ColorAsset(name: "ScoreSheet/Text/OnRail/Pride")
+        }
       }
     }
     public enum Secondary {
