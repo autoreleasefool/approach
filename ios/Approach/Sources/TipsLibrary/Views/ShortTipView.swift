0fc393596bf56b771264ffbee5fd3d563786675c
Joseph Roque <github@josephroque.ca>
2023-09-16 22:53:38 -0700

    Feat: Shorter tips with no message

---
+++ ios/Approach/Sources/TipsLibrary/Views/ShortTipView.swift
@@ -0,0 +1,42 @@
+import AssetsLibrary
+import SwiftUI
+import ViewsLibrary
+
+public struct ShortTipView: View {
+	let tip: Tip
+	let onDismiss: () -> Void
+
+	public init(tip: Tip, onDismiss: @escaping () -> Void) {
+		self.tip = tip
+		self.onDismiss = onDismiss
+	}
+
+	public var body: some View {
+		HStack(alignment: .center, spacing: 0) {
+			Text(tip.title)
+				.font(.caption)
+				.frame(maxWidth: .infinity, alignment: .leading)
+			Button(action: onDismiss) {
+				Image(systemSymbol: .xmark)
+					.resizable()
+					.scaledToFit()
+					.frame(width: .smallIcon, height: .smallSpacing)
+					.padding(.leading)
+			}
+			.buttonStyle(TappableElement())
+		}
+		.frame(maxWidth: .infinity)
+	}
+}
+
+#if DEBUG
+struct ShortTipViewPreview: PreviewProvider {
+	static var previews: some View {
+		List {
+			Section {
+				ShortTipView(tip: .init(title: "This is a short and sweet tip")) { }
+			}
+		}
+	}
+}
+#endif
