1e2c6b89d53f7c74be92d51fbec7a32cdbe99b07
Joseph Roque <github@josephroque.ca>
2023-09-21 13:56:00 -0700

    Refactor: Efficiently calculate frame changes swiping pin, send less actions

--- ios/Approach/Sources/GamesEditorFeature/GamesEditor+FrameEditor.swift
+++ ios/Approach/Sources/GamesEditorFeature/GamesEditor+FrameEditor.swift
@@ -1,20 +1,28 @@
 import ComposableArchitecture
+import ModelsLibrary
 
 extension GamesEditor.State {
 	var frameEditor: FrameEditor.State? {
 		get {
 			guard let _frameEditor, let frames else { return nil }
+			let frame = frames[currentFrameIndex]
 			var frameEditor = _frameEditor
 			frameEditor.isEditable = isEditable
-			frameEditor.currentRollIndex = currentRollIndex
-			frameEditor.frame = frames[currentFrameIndex]
+			frameEditor.downedPins = frame.deck(forRoll: currentRollIndex)
+			let pinsDownLastFrame = currentRollIndex > 0 ? frame.deck(forRoll: currentRollIndex - 1) : []
+			if Frame.isLast(frame.index) {
+				frameEditor.lockedPins = pinsDownLastFrame.arePinsCleared ? [] : pinsDownLastFrame
+			} else {
+				frameEditor.lockedPins = pinsDownLastFrame
+			}
 			return frameEditor
 		}
 		set {
 			_frameEditor = newValue
 			guard let newValue else { return }
 			let currentFrameIndex = self.currentFrameIndex
-			self.frames?[currentFrameIndex] = newValue.frame
+			let currentRollIndex = self.currentRollIndex
+			self.frames?[currentFrameIndex].setDownedPins(rollIndex: currentRollIndex, to: newValue.downedPins)
 			self.setCurrent()
 		}
 	}
