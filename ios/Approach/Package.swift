d4c0627749ea99f39db07af07760005449dbde2c
Joseph Roque <github@josephroque.ca>
2023-11-01 10:46:14 -0700

    Chore: Bump PopupView to 2.8.2

--- ios/Approach/Package.swift
+++ ios/Approach/Package.swift
@@ -149,7 +149,7 @@ let package = Package(
 		.package(url: "https://github.com/apple/swift-algorithms.git", from: "1.1.0"),
 		.package(url: "https://github.com/apple/swift-async-algorithms.git", from: "0.1.0"),
 		.package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.8.2"),
-		.package(url: "https://github.com/exyte/PopupView.git", from: "2.6.0"),
+		.package(url: "https://github.com/exyte/PopupView.git", from: "2.8.2"),
 		.package(url: "https://github.com/groue/GRDB.swift.git", from: "6.21.0"),
 		.package(url: "https://github.com/markiv/SwiftUI-Shimmer.git", from: "1.4.0"),
 		.package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.2.0"),
