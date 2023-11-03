b9acb30d9b1cf3b952b4c222877e066eecfb0b55
Joseph Roque <github@josephroque.ca>
2023-11-03 16:08:57 -0700

    Feat: Replace OutlinedCard with Card in Series and Games lists

--- android/app/src/main/java/ca/josephroque/bowlingcompanion/feature/seriesdetails/ui/SeriesDetailsHeader.kt
+++ android/app/src/main/java/ca/josephroque/bowlingcompanion/feature/seriesdetails/ui/SeriesDetailsHeader.kt
@@ -6,8 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -41,7 +41,7 @@ fun SeriesDetailsHeader(
 	scores: ChartEntryModel?,
 	modifier: Modifier = Modifier,
 ) {
-	ElevatedCard(
+	Card(
 		modifier = modifier
 			.fillMaxWidth(),
 		elevation = CardDefaults.elevatedCardElevation(),
--- android/app/src/main/java/ca/josephroque/bowlingcompanion/feature/serieslist/SeriesItemRow.kt
+++ android/app/src/main/java/ca/josephroque/bowlingcompanion/feature/serieslist/SeriesItemRow.kt
@@ -10,10 +10,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -59,7 +59,7 @@ internal fun SeriesItemRow(
 	onClick: () -> Unit,
 	modifier: Modifier = Modifier,
 ) {
-	OutlinedCard(
+	Card(
 		onClick = onClick,
 		modifier = modifier.padding(horizontal = 16.dp),
 	) {
