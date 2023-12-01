8499c19fb1cd6ea88eaa471c1af6d19fe41f697e
Joseph Roque <contact@josephroque.ca>
2023-12-01 01:56:32 -0800

    fix: add new statistics to full list

--- android/core/statistics/src/main/java/ca/josephroque/bowlingcompanion/core/statistics/Statistics.kt
+++ android/core/statistics/src/main/java/ca/josephroque/bowlingcompanion/core/statistics/Statistics.kt
@@ -1,12 +1,63 @@
 package ca.josephroque.bowlingcompanion.core.statistics
 
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.AcesSparedStatistic
 import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.AcesStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.ChopOffsSparedStatistic
 import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.ChopOffsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.FivesSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.FivesStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.HeadPinsSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.HeadPinsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.LeftChopOffsSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.LeftChopOffsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.LeftFivesSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.LeftFivesStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.LeftSplitsSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.LeftSplitsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.LeftTapsSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.LeftTapsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.LeftThreesSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.LeftThreesStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.LeftTwelvesSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.LeftTwelvesStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.RightChopOffsSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.RightChopOffsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.RightFivesSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.RightFivesStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.RightSplitsSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.RightSplitsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.RightTapsSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.RightTapsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.RightThreesSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.RightThreesStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.RightTwelvesSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.RightTwelvesStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.SplitsSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.SplitsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.TapsSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.TapsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.ThreesSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.ThreesStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.TwelvesSparedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.firstroll.TwelvesStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.foul.FoulsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.mark.SpareConversionsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.mark.StrikesStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.matchplay.MatchesLostStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.matchplay.MatchesPlayedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.matchplay.MatchesTiedStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.matchplay.MatchesWonStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.middlehit.LeftOfMiddleHitsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.middlehit.MiddleHitsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.middlehit.RightOfMiddleHitsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.middlehit.StrikeMiddleHitsStatistic
 import ca.josephroque.bowlingcompanion.core.statistics.trackable.overall.GameAverageStatistic
 import ca.josephroque.bowlingcompanion.core.statistics.trackable.overall.HighSingleStatistic
 import ca.josephroque.bowlingcompanion.core.statistics.trackable.overall.NumberOfGamesStatistic
 import ca.josephroque.bowlingcompanion.core.statistics.trackable.overall.TotalPinFallStatistic
 import ca.josephroque.bowlingcompanion.core.statistics.trackable.overall.TotalRollsStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.pinsleft.AveragePinsLeftOnDeckStatistic
+import ca.josephroque.bowlingcompanion.core.statistics.trackable.pinsleft.TotalPinsLeftOnDeckStatistic
 import ca.josephroque.bowlingcompanion.core.statistics.trackable.series.HighSeriesOf3Statistic
 
 fun allStatistics(
@@ -20,11 +71,84 @@ fun allStatistics(
 	GameAverageStatistic(),
 	TotalRollsStatistic(),
 
+	// Middle Hits
+	MiddleHitsStatistic(),
+	LeftOfMiddleHitsStatistic(),
+	RightOfMiddleHitsStatistic(),
+	StrikeMiddleHitsStatistic(),
+
+	// Strikes and Spares
+	StrikesStatistic(),
+	SpareConversionsStatistic(),
+
+	// Head Pins
+	HeadPinsStatistic(),
+	HeadPinsSparedStatistic(),
+
+	// Fives
+	FivesStatistic(),
+	FivesSparedStatistic(),
+	LeftFivesStatistic(),
+	LeftFivesSparedStatistic(),
+	RightFivesStatistic(),
+	RightFivesSparedStatistic(),
+
+	// Threes
+	ThreesStatistic(),
+	ThreesSparedStatistic(),
+	LeftThreesStatistic(),
+	LeftThreesSparedStatistic(),
+	RightThreesStatistic(),
+	RightThreesSparedStatistic(),
+
+	// Taps
+	TapsStatistic(),
+	TapsSparedStatistic(),
+	LeftTapsStatistic(),
+	LeftTapsSparedStatistic(),
+	RightTapsStatistic(),
+	RightTapsSparedStatistic(),
+
 	// Aces
 	AcesStatistic(),
+	AcesSparedStatistic(),
 
 	// Chop offs
 	ChopOffsStatistic(),
+	ChopOffsSparedStatistic(),
+	LeftChopOffsStatistic(),
+	LeftChopOffsSparedStatistic(),
+	RightChopOffsStatistic(),
+	RightChopOffsSparedStatistic(),
+
+	// Splits
+	SplitsStatistic(),
+	SplitsSparedStatistic(),
+	LeftSplitsStatistic(),
+	LeftSplitsSparedStatistic(),
+	RightSplitsStatistic(),
+	RightSplitsSparedStatistic(),
+
+	// Twelves
+	TwelvesStatistic(),
+	TwelvesSparedStatistic(),
+	LeftTwelvesStatistic(),
+	LeftTwelvesSparedStatistic(),
+	RightTwelvesStatistic(),
+	RightTwelvesSparedStatistic(),
+
+	// Fouls
+	FoulsStatistic(),
+
+	// Pins Left
+	TotalPinsLeftOnDeckStatistic(),
+	AveragePinsLeftOnDeckStatistic(),
+
+	// Match Play
+	MatchesPlayedStatistic(),
+	MatchesWonStatistic(),
+	MatchesLostStatistic(),
+	MatchesTiedStatistic(),
 
 	// Series
 	HighSeriesOf3Statistic(),
