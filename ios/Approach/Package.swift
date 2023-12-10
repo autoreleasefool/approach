31e931a4abcabf5d2be1fd48a47836cb677f253f
Joseph Roque <contact@josephroque.ca>
2023-12-10 12:53:03 -0800

    chore(ios): bump revenuecat to 4.31.2

--- ios/Approach/Package.swift
+++ ios/Approach/Package.swift
@@ -165,7 +165,7 @@ let package = Package(
 		.package(url: "https://github.com/pointfreeco/swift-identified-collections.git", from: "1.0.0"),
 		.package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.14.2"),
 		.package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay.git", from: "1.0.2"),
-		.package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "4.25.9"),
+		.package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "4.31.2"),
 		.package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", from: "4.1.1"),
 		.package(url: "https://github.com/TelemetryDeck/SwiftClient.git", from: "1.4.4"),
 		.package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.16"),
