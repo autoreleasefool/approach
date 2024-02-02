819cad3b503544ebd36b8e7af7905bbbf1871742
Joseph Roque <contact@josephroque.ca>
2024-02-01 20:20:20 -0800

    chore(ios): bump ZIPFoundation to 0.9.18

--- ios/Approach/Package.swift
+++ ios/Approach/Package.swift
@@ -169,7 +169,7 @@ let package = Package(
 		.package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "4.31.3"),
 		.package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", from: "4.1.1"),
 		.package(url: "https://github.com/TelemetryDeck/SwiftClient.git", from: "1.5.1"),
-		.package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.17"),
+		.package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.18"),
 	],
 	targets: [
 		// MARK: - Features
