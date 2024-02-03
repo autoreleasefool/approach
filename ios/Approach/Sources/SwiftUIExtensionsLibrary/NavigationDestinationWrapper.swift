b3be2fed23ce2ae5f83711949a705e92c8a17b03
Joseph Roque <contact@josephroque.ca>
2024-02-02 22:53:55 -0800

    fix(ios): Repeated navigation works

--- ios/Approach/Sources/SwiftUIExtensionsLibrary/NavigationDestinationWrapper.swift
+++ ios/Approach/Sources/SwiftUIExtensionsLibrary/NavigationDestinationWrapper.swift
@@ -1,28 +1,26 @@
 import SwiftUI
 
 extension View {
+	@available(iOS, introduced: 16, deprecated: 17)
+	@available(macOS, introduced: 13, deprecated: 14)
+	@available(tvOS, introduced: 16, deprecated: 17)
+	@available(watchOS, introduced: 9, deprecated: 10)
 	@ViewBuilder
 	public func navigationDestinationWrapper<D: Hashable, C: View>(
 		item: Binding<D?>,
 		@ViewBuilder destination: @escaping (D) -> C
 	) -> some View {
-		if #available(iOS 17, macOS 14, tvOS 17, visionOS 1, watchOS 10, *) {
-			navigationDestination(item: item, destination: destination)
-		} else {
-			navigationDestination(
-				isPresented: Binding(
-					get: { item.wrappedValue != nil },
-					set: { isPresented, transaction in
-						if !isPresented {
-							item.transaction(transaction).wrappedValue = nil
-						}
-					}
-				)
-			) {
-				if let item = item.wrappedValue {
-					destination(item)
-				}
+		navigationDestination(isPresented: item.isPresented) {
+			if let item = item.wrappedValue {
+				destination(item)
 			}
 		}
+	}
+}
+
+fileprivate extension Optional where Wrapped: Hashable {
+	var isPresented: Bool {
+		get { self != nil }
+		set { if !newValue { self = nil } }
 	}
 }
