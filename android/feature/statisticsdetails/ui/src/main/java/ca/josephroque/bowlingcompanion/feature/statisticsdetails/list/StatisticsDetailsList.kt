2b731cca0f9ff579aea8ea0190c734d115db8c3b
Joseph Roque <contact@josephroque.ca>
2024-02-12 20:02:39 -0800

    feat(android): list statistics filter properties

--- android/feature/statisticsdetails/ui/src/main/java/ca/josephroque/bowlingcompanion/feature/statisticsdetails/list/StatisticsDetailsList.kt
+++ android/feature/statisticsdetails/ui/src/main/java/ca/josephroque/bowlingcompanion/feature/statisticsdetails/list/StatisticsDetailsList.kt
@@ -1,16 +1,25 @@
 package ca.josephroque.bowlingcompanion.feature.statisticsdetails.list
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
@@ -18,6 +27,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -25,13 +35,29 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
+import ca.josephroque.bowlingcompanion.core.common.utils.simpleFormat
 import ca.josephroque.bowlingcompanion.core.designsystem.components.LabeledSwitch
 import ca.josephroque.bowlingcompanion.core.designsystem.components.state.MutedEmptyState
 import ca.josephroque.bowlingcompanion.core.designsystem.theme.ApproachTheme
+import ca.josephroque.bowlingcompanion.core.model.BowlerSummary
+import ca.josephroque.bowlingcompanion.core.model.GameSummary
+import ca.josephroque.bowlingcompanion.core.model.LeagueRecurrence
+import ca.josephroque.bowlingcompanion.core.model.LeagueSummary
+import ca.josephroque.bowlingcompanion.core.model.SeriesSummary
+import ca.josephroque.bowlingcompanion.core.model.TrackableFilter
+import ca.josephroque.bowlingcompanion.core.model.stub.BowlerSummaryStub
+import ca.josephroque.bowlingcompanion.core.model.stub.LeagueSummaryStub
+import ca.josephroque.bowlingcompanion.core.model.ui.title
 import ca.josephroque.bowlingcompanion.core.statistics.StatisticID
 import ca.josephroque.bowlingcompanion.core.statistics.models.StatisticListEntry
 import ca.josephroque.bowlingcompanion.core.statistics.models.StatisticListEntryGroup
 import ca.josephroque.bowlingcompanion.feature.statisticsdetails.ui.R
+import java.util.UUID
+
+private val FILTER_ITEM_WIDTH = 180.dp
+private fun Modifier.filterItem() =
+	fillMaxHeight()
+	.widthIn(max = FILTER_ITEM_WIDTH)
 
 @Composable
 fun StatisticsDetailsList(
@@ -50,9 +76,47 @@ fun StatisticsDetailsList(
 					title = R.string.statistics_list_empty_title,
 					icon = R.drawable.statistics_list_empty_state,
 					message = R.string.statistics_list_empty_message,
+					modifier = Modifier
+						.padding(vertical = 16.dp)
+						.onGloballyPositioned {
+							onAction(StatisticsDetailsListUiAction.HeaderHeightMeasured(it.size.height.toFloat()))
+						},
 				)
 			}
+
+			if (state.filterSources != null) {
+				item {
+					FilterDetails(
+						filter = state.filter,
+						filterSource = state.filterSources,
+						modifier = Modifier.padding(bottom = 16.dp),
+					)
+				}
+			}
 		} else {
+			item {
+				Column(
+					modifier = Modifier
+						.onGloballyPositioned {
+							onAction(StatisticsDetailsListUiAction.HeaderHeightMeasured(it.size.height.toFloat()))
+						},
+				) {
+					Text(
+						text = stringResource(R.string.statistics_filter_title),
+						style = MaterialTheme.typography.titleLarge,
+						modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+					)
+
+					if (state.filterSources != null) {
+						FilterDetails(
+							filter = state.filter,
+							filterSource = state.filterSources,
+							modifier = Modifier.padding(bottom = 16.dp),
+						)
+					}
+				}
+			}
+
 			items(
 				items = state.statistics,
 				key = { it.title }
@@ -75,8 +139,218 @@ fun StatisticsDetailsList(
 				modifier = Modifier
 					.padding(bottom = 16.dp)
 					.padding(horizontal = 16.dp),
-
 			)
+		}
+	}
+}
+
+@Composable
+private fun FilterDetails(
+	filter: TrackableFilter,
+	filterSource: TrackableFilter.SourceSummaries,
+	modifier: Modifier = Modifier,
+) {
+	Row(
+		horizontalArrangement = Arrangement.spacedBy(8.dp),
+		modifier = modifier
+			.horizontalScroll(rememberScrollState())
+			.height(intrinsicSize = IntrinsicSize.Max)
+	) {
+		Spacer(modifier = Modifier.width(8.dp))
+
+		BowlerFilters(
+			bowler = filterSource.bowler,
+		)
+
+		LeagueFilters(
+			league = filterSource.league,
+			filter = filter,
+		)
+
+		SeriesFilters(
+			series = filterSource.series,
+			filter = filter,
+		)
+
+		GameFilters(
+			game = filterSource.game,
+			filter = filter,
+		)
+
+		FrameFilters(
+			filter = filter,
+		)
+
+		Spacer(modifier = Modifier.width(8.dp))
+	}
+}
+
+@Composable
+private fun BowlerFilters(
+	bowler: BowlerSummary,
+) {
+	FilterItem(
+		label = stringResource(R.string.statistics_filter_label_bowler),
+		text = bowler.name,
+		modifier = Modifier.filterItem(),
+	)
+}
+
+@Composable
+private fun LeagueFilters(
+	league: LeagueSummary?,
+	filter: TrackableFilter,
+) {
+	if (league != null) {
+		FilterItem(
+			label = stringResource(R.string.statistics_filter_label_league),
+			text = league.name,
+			modifier = Modifier.filterItem(),
+		)
+	} else if (filter.leagues.recurrence != null) {
+		FilterItem(
+			label = stringResource(R.string.statistics_filter_label_league_recurrence),
+			text = when (filter.leagues.recurrence!!) {
+				LeagueRecurrence.REPEATING -> stringResource(R.string.statistics_filter_label_league_recurrence_repeats)
+				LeagueRecurrence.ONCE -> stringResource(R.string.statistics_filter_label_league_recurrence_never)
+			},
+			modifier = Modifier.filterItem(),
+		)
+	}
+}
+
+@Composable
+private fun SeriesFilters(
+	series: SeriesSummary?,
+	filter: TrackableFilter,
+) {
+	if (series != null) {
+		FilterItem(
+			label = stringResource(R.string.statistics_filter_label_series),
+			text = series.date.simpleFormat(),
+			modifier = Modifier.filterItem(),
+		)
+	} else {
+		if (filter.series.startDate != null) {
+			FilterItem(
+				label = stringResource(R.string.statistics_filter_label_series_starts),
+				text = filter.series.startDate!!.simpleFormat(),
+				modifier = Modifier.filterItem(),
+			)
+		}
+
+		if (filter.series.endDate != null) {
+			FilterItem(
+				label = stringResource(R.string.statistics_filter_label_series_ends),
+				text = filter.series.endDate!!.simpleFormat(),
+				modifier = Modifier.filterItem(),
+			)
+		}
+
+		when (val alley = filter.series.alleys) {
+			is TrackableFilter.AlleyFilter.Alley -> Unit // FIXME: show alley name
+			is TrackableFilter.AlleyFilter.Properties -> {
+				if (alley.material != null) {
+					FilterItem(
+						label = stringResource(R.string.statistics_filter_label_series_alley_material),
+						text = alley.material?.title() ?: "",
+						modifier = Modifier.filterItem(),
+					)
+				}
+
+				if (alley.mechanism != null) {
+					FilterItem(
+						label = stringResource(R.string.statistics_filter_label_series_alley_mechanism),
+						text = alley.mechanism?.title() ?: "",
+						modifier = Modifier.filterItem(),
+					)
+				}
+
+				if (alley.pinFall != null) {
+					FilterItem(
+						label = stringResource(R.string.statistics_filter_label_series_alley_pin_fall),
+						text = alley.pinFall?.title() ?: "",
+						modifier = Modifier.filterItem(),
+					)
+				}
+
+				if (alley.pinBase != null) {
+					FilterItem(
+						label = stringResource(R.string.statistics_filter_label_series_alley_pin_base),
+						text = alley.pinBase?.title() ?: "",
+						modifier = Modifier.filterItem(),
+					)
+				}
+			}
+			null -> Unit
+		}
+	}
+}
+
+@Composable
+private fun GameFilters(
+	game: GameSummary?,
+	filter: TrackableFilter,
+) {
+	if (game != null) {
+		FilterItem(
+			label = stringResource(R.string.statistics_filter_label_game),
+			text = stringResource(ca.josephroque.bowlingcompanion.core.designsystem.R.string.game_with_ordinal, game.index + 1),
+			modifier = Modifier.filterItem(),
+		)
+	} else {
+		// FIXME: show lanes
+//		if (filter.games.lanes != null) {
+//		}
+
+		// FIXME: show gear used
+//		if (filter.games.gearUsed.isNotEmpty()) {
+//		}
+
+		// FIXME: show opponent name
+//		if (filter.games.opponent != null) {
+//		}
+	}
+}
+
+@Composable
+private fun FrameFilters(
+	filter: TrackableFilter,
+) {
+//	if (filter.frames.bowlingBallsUsed.isNotEmpty()) {
+		// FIXME: Show gear used
+//	}
+}
+
+@Composable
+private fun FilterItem(
+	label: String,
+	modifier: Modifier = Modifier,
+	text: String? = null,
+) {
+	Card(
+		modifier = modifier,
+		colors = CardDefaults.cardColors(
+			containerColor = MaterialTheme.colorScheme.primaryContainer,
+		),
+	) {
+		Column(
+			verticalArrangement = Arrangement.spacedBy(2.dp),
+			modifier = Modifier.padding(8.dp),
+		) {
+			Text(
+				text = label,
+				style = MaterialTheme.typography.labelMedium,
+			)
+
+			if (text != null) {
+				Text(
+					text = text,
+					style = MaterialTheme.typography.bodyMedium,
+				)
+			}
+
+			Spacer(modifier = Modifier.weight(1f))
 		}
 	}
 }
@@ -299,6 +573,13 @@ private fun StatisticsDetailsListPreview(
 		Surface {
 			StatisticsDetailsList(
 				state = StatisticsDetailsListUiState(
+					filter = TrackableFilter(source = TrackableFilter.Source.Bowler(UUID.randomUUID())),
+					filterSources = TrackableFilter.SourceSummaries(
+						bowler = BowlerSummaryStub.single(),
+						league = LeagueSummaryStub.single(),
+						series = null,
+						game = null,
+					),
 					statistics = statistics,
 				),
 				onAction = {},
--- android/feature/statisticsdetails/ui/src/main/java/ca/josephroque/bowlingcompanion/feature/statisticsdetails/list/StatisticsDetailsListUi.kt
+++ android/feature/statisticsdetails/ui/src/main/java/ca/josephroque/bowlingcompanion/feature/statisticsdetails/list/StatisticsDetailsListUi.kt
@@ -1,9 +1,12 @@
 package ca.josephroque.bowlingcompanion.feature.statisticsdetails.list
 
+import ca.josephroque.bowlingcompanion.core.model.TrackableFilter
 import ca.josephroque.bowlingcompanion.core.statistics.StatisticID
 import ca.josephroque.bowlingcompanion.core.statistics.models.StatisticListEntryGroup
 
 data class StatisticsDetailsListUiState(
+	val filter: TrackableFilter,
+	val filterSources: TrackableFilter.SourceSummaries? = null,
 	val statistics: List<StatisticListEntryGroup> = emptyList(),
 	val highlightedEntry: StatisticID? = null,
 	val isHidingZeroStatistics: Boolean = true,
@@ -14,4 +17,5 @@ sealed interface StatisticsDetailsListUiAction {
 	data class StatisticClicked(val id: StatisticID): StatisticsDetailsListUiAction
 	data class HidingZeroStatisticsToggled(val newValue: Boolean): StatisticsDetailsListUiAction
 	data class HidingStatisticDescriptionsToggled(val newValue: Boolean): StatisticsDetailsListUiAction
+	data class HeaderHeightMeasured(val height: Float): StatisticsDetailsListUiAction
 }
