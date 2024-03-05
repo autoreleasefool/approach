8a7755f6e1c3614f968e5880e49ec65c10728e2c
Joseph Roque <contact@josephroque.ca>
2024-03-04 22:17:44 -0800

    chore(ios): bump SFSafeSymbols to 5.2.0

--- ios/Approach/Package.swift
+++ ios/Approach/Package.swift
@@ -170,7 +170,7 @@ let package = Package(
 		.package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.15.2"),
 		.package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay.git", from: "1.1.0"),
 		.package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "4.37.0"),
-		.package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", from: "4.1.1"),
+		.package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", from: "5.2.0"),
 		.package(url: "https://github.com/TelemetryDeck/SwiftClient.git", from: "1.5.1"),
 		.package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.18"),
 	],
