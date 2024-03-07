dd72c2c98e2c1448ef1d9cc9f91076506e9c428b
Joseph Roque <contact@josephroque.ca>
2024-03-06 23:03:36 -0800

    chore(android): bump android gradle plugin to 8.3.0

--- android/build-logic/convention/src/main/kotlin/ca/josephroque/bowlingcompanion/KotlinAndroid.kt
+++ android/build-logic/convention/src/main/kotlin/ca/josephroque/bowlingcompanion/KotlinAndroid.kt
@@ -12,7 +12,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 /**
  * Configure base Kotlin with Android options
  */
-internal fun Project.configureKotlinAndroid(commonExtension: CommonExtension<*, *, *, *, *>) {
+internal fun Project.configureKotlinAndroid(commonExtension: CommonExtension<*, *, *, *, *, *>) {
 	commonExtension.apply {
 		compileSdk = libs.findVersion("compileSdk").get().toString().toInt()
 
