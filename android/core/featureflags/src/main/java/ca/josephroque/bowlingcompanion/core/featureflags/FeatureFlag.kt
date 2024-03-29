3a8044e22ea84ffa2fce4063d607b14a94dafeb3
Joseph Roque <contact@josephroque.ca>
2024-03-28 19:01:09 -0700

    chore(android): rollout MANUAL_SERIES_FORM to RELEASE

--- android/core/featureflags/src/main/java/ca/josephroque/bowlingcompanion/core/featureflags/FeatureFlag.kt
+++ android/core/featureflags/src/main/java/ca/josephroque/bowlingcompanion/core/featureflags/FeatureFlag.kt
@@ -22,7 +22,7 @@ enum class FeatureFlag(
 	DATA_EXPORT("DataExport", "2023-10-12", RolloutStage.RELEASE),
 	DATA_IMPORT("DataImport", "2023-10-13", RolloutStage.RELEASE),
 	PRE_BOWL_FORM("PreBowlForm", "2024-03-24", RolloutStage.RELEASE),
-	MANUAL_SERIES_FORM("ManualSeriesForm", "2024-03-28", RolloutStage.DEVELOPMENT),
+	MANUAL_SERIES_FORM("ManualSeriesForm", "2024-03-28", RolloutStage.RELEASE),
 }
 
 fun FeatureFlag.isEnabled(): Boolean = if (BuildConfig.DEBUG) {
