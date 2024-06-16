c02d31a09b84a8182bdd7d234fd6f36dbc5c6c60
Joseph Roque <contact@josephroque.ca>
2024-06-15 17:25:52 -0700

    chore(ios): enable sharing feature flags

--- ios/Approach/Sources/FeatureFlagsLibrary/FeatureFlags.swift
+++ ios/Approach/Sources/FeatureFlagsLibrary/FeatureFlags.swift
@@ -6,15 +6,15 @@ extension FeatureFlag {
 	public static let developerOptions = Self(name: "developerOptions", introduced: "2022-11-10", stage: .development, isOverridable: false)
 	public static let teams = Self(name: "teams", introduced: "2023-01-03", stage: .development)
 	public static let opponentDetails = Self(name: "opponentDetails", introduced: "2023-08-18", stage: .development)
-	public static let sharingGame = Self(name: "sharingGame", introduced: "2023-08-29", stage: .development)
-	public static let sharingSeries = Self(name: "sharingSeries", introduced: "2023-08-29", stage: .development)
+	public static let sharingGame = Self(name: "sharingGame", introduced: "2023-08-29", stage: .release)
+	public static let sharingSeries = Self(name: "sharingSeries", introduced: "2023-08-29", stage: .release)
 	public static let alleyAndGearAverages = Self(name: "alleyAndGearAverages", introduced: "2023-09-12", stage: .development)
 	public static let proSubscription = Self(name: "proSubscription", introduced: "2023-08-08", stage: .development)
 	public static let purchases = Self(name: "purchase", introduced: "2023-09-15", stage: .disabled)
 	public static let dataImport = Self(name: "dataImport", introduced: "2023-09-17", stage: .development)
 	public static let preBowlForm = Self(name: "preBowlForm", introduced: "2024-03-23", stage: .release)
 	public static let manualSeries = Self(name: "manualSeries", introduced: "2024-03-28", stage: .release)
-	public static let sharingStatistic = Self(name: "sharingStatistic", introduced: "2024-04-27", stage: .development)
+	public static let sharingStatistic = Self(name: "sharingStatistic", introduced: "2024-04-27", stage: .release)
 
 	public static let allFlags: [Self] = [
 		.alleyAndGearAverages,
