f299d6a6d8ace13e0f009ba5a0843a241bc6f571
Joseph Roque <contact@josephroque.ca>
2024-06-17 18:54:40 -0700

    feat(android): material theme, with google fonts

--- android/core/designsystem/build.gradle.kts
+++ android/core/designsystem/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
 	api(libs.androidx.compose.foundation.layout)
 	api(libs.androidx.compose.material3)
 	api(libs.androidx.compose.runtime)
+	api(libs.androidx.compose.ui.google.fonts)
 	api(libs.androidx.compose.ui.tooling.preview)
 	api(libs.androidx.core.ktx)
 
