f43d66b85d6cc5d2a030b066457ca74278b41564
Joseph Roque <contact@josephroque.ca>
2024-06-22 19:15:25 -0700

    fix(android): fix IllegalStateException for FocusRequester by removing

--- android/feature/onboarding/ui/src/main/java/ca/josephroque/bowlingcompanion/feature/onboarding/ui/newuser/components/Logbook.kt
+++ android/feature/onboarding/ui/src/main/java/ca/josephroque/bowlingcompanion/feature/onboarding/ui/newuser/components/Logbook.kt
@@ -1,8 +1,8 @@
 package ca.josephroque.bowlingcompanion.feature.onboarding.ui.newuser.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -13,19 +13,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ca.josephroque.bowlingcompanion.feature.onboarding.ui.R
 import ca.josephroque.bowlingcompanion.feature.onboarding.ui.newuser.NewUserOnboardingUiAction
@@ -45,12 +44,10 @@ fun Logbook(
 			}
 		},
 	)
-	val focusRequester = remember { FocusRequester() }
-	val focusManager = LocalFocusManager.current
 
-	LaunchedEffect(Unit) {
-		focusRequester.requestFocus()
-	}
+	// TODO: Figure out a way to focus text field automatically without crashing
+	// IllegalStateException: FocusRequester is not initialized.
+	val focusManager = LocalFocusManager.current
 
 	ModalBottomSheet(
 		onDismissRequest = {},
@@ -70,7 +67,7 @@ fun Logbook(
 		val textStyle = localStyle.merge(
 			TextStyle(color = LocalContentColor.current, textAlign = TextAlign.Center),
 		)
-		BasicTextField(
+		TextField(
 			value = name,
 			onValueChange = { onAction(NewUserOnboardingUiAction.NameChanged(it)) },
 			textStyle = textStyle,
@@ -85,10 +82,19 @@ fun Logbook(
 					onAction(NewUserOnboardingUiAction.AddBowlerClicked)
 				},
 			),
+			placeholder = {
+				Box(modifier = Modifier.fillMaxWidth()) {
+					Text(
+						text = stringResource(R.string.onboarding_new_user_your_name),
+						style = MaterialTheme.typography.bodyLarge,
+						textAlign = TextAlign.Center,
+						modifier = Modifier.align(Alignment.Center),
+					)
+				}
+			},
 			modifier = Modifier
 				.fillMaxWidth()
-				.padding(horizontal = 16.dp)
-				.focusRequester(focusRequester),
+				.padding(horizontal = 16.dp),
 		)
 
 		Button(
@@ -106,4 +112,13 @@ fun Logbook(
 			)
 		}
 	}
+}
+
+@Preview
+@Composable
+private fun LogbookPreview() {
+	Logbook(
+		name = "Joseph Roque",
+		onAction = {},
+	)
 }
