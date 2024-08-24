4f85112bb438bbb5c85032232446b7be11dc23cc
Joseph Roque <contact@josephroque.ca>
2024-08-23 17:15:51 -0700

    fix(android): modules should be Singleton

--- android/core/analytics/src/main/java/ca/josephroque/bowlingcompanion/core/analytics/di/AnalyticsModule.kt
+++ android/core/analytics/src/main/java/ca/josephroque/bowlingcompanion/core/analytics/di/AnalyticsModule.kt
@@ -6,10 +6,12 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 interface AnalyticsModule {
+	@Singleton
 	@Binds
 	fun bindsAnalyticsClient(analyticsClient: TelemetryDeckAnalyticsClient): AnalyticsClient
 }
