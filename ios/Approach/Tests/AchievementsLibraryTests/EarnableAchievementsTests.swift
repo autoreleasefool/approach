5c2d8d67022a51e058832b16907e8f310d3db951
Joseph Roque <contact@josephroque.ca>
2025-03-20 22:43:06 -0700

    test(ios): AchievementsLibrary test coverage

--- ios/Approach/Tests/AchievementsLibraryTests/EarnableAchievementsTests.swift
+++ ios/Approach/Tests/AchievementsLibraryTests/EarnableAchievementsTests.swift
@@ -12,4 +12,53 @@ struct EarnableAchievementsTests {
 
 		#expect(allAchievements.count == allAchievementTitles.count)
 	}
+
+	@Test("Achievements are findable by title", .tags(.unit))
+	func achievementsAreFindableByTitle() {
+		let allAchievements = EarnableAchievements.allCases
+		let allAchievementTitles = allAchievements.map { $0.title }
+
+		for title in allAchievementTitles {
+			#expect(EarnableAchievements.allCasesByTitle[title] != nil)
+		}
+	}
+
+	@Test("Achievements are enabled correctly", .tags(.unit))
+	func achievementsAreEnabledCorrectly() {
+		let enabledAchievements = EarnableAchievements.allCases
+			.filter { $0.isEnabled }
+			.map { $0.title }
+
+		let expectedAchievements = [
+			EarnableAchievements.TenYears.self,
+		].map { $0.title }
+
+		#expect(enabledAchievements == expectedAchievements)
+	}
+
+	@Test("Achievements are not visible before earned", .tags(.unit))
+	func achievementsAreNotVisibleBeforeEarned() {
+		let visibleAchievements = EarnableAchievements.allCases
+			.filter { $0.isVisibleBeforeEarned }
+			.map { $0.title }
+
+		let expectedAchievements = [
+			EarnableAchievements.Iconista.self,
+		].map { $0.title }
+
+		#expect(visibleAchievements == expectedAchievements)
+	}
+
+	@Test("Achievements are not shown on earn", .tags(.unit))
+	func achievementsAreNotShownOnEarn() {
+		let shownAchievements = EarnableAchievements.allCases
+			.filter { $0.showToastOnEarn }
+			.map { $0.title }
+
+		let expectedAchievements = [
+			EarnableAchievements.Iconista.self,
+		].map { $0.title }
+
+		#expect(shownAchievements == expectedAchievements)
+	}
 }
