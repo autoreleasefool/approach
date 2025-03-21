c0880470b902a1f3bce7b1a4aaeaf59d25c36fea
Joseph Roque <contact@josephroque.ca>
2025-03-21 00:13:09 -0700

    fix(ios): index achievement events by title

--- ios/Approach/Sources/DatabaseMigrationsLibrary/Migrations/Migration20250315CreateAchievements.swift
+++ ios/Approach/Sources/DatabaseMigrationsLibrary/Migrations/Migration20250315CreateAchievements.swift
@@ -3,22 +3,23 @@ import GRDBDatabasePackageLibrary
 
 struct Migration20250315CreateAchievements: Migration {
 	static func migrate(_ db: Database) throws {
+		try db.create(table: "achievementEvent") { t in
+			t.column("id", .text)
+				.primaryKey()
+			t.column("title", .text)
+				.indexed()
+				.notNull()
+			t.column("isConsumed", .boolean)
+				.defaults(to: false)
+				.notNull()
+		}
+
 		try db.create(table: "achievement") { t in
 			t.column("id", .text)
 				.primaryKey()
 			t.column("title", .text)
 				.notNull()
 			t.column("earnedAt", .datetime)
-				.notNull()
-		}
-
-		try db.create(table: "achievementEvent") { t in
-			t.column("id", .text)
-				.primaryKey()
-			t.column("title", .text)
-				.notNull()
-			t.column("isConsumed", .boolean)
-				.defaults(to: false)
 				.notNull()
 		}
 	}
