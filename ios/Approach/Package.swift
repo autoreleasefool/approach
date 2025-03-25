fcdd3ea582f581b66a32dd661ec2146b468366f4
Joseph Roque <contact@josephroque.ca>
2025-03-24 23:53:44 -0700

    chore(ios): bump swift-algorithms to 1.2.1

--- ios/Approach/Package.swift
+++ ios/Approach/Package.swift
@@ -156,7 +156,7 @@ let package = Package(
 		.library(name: "ViewsLibrary", targets: ["ViewsLibrary"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/apple/swift-algorithms.git", from: "1.2.0"),
+		.package(url: "https://github.com/apple/swift-algorithms.git", from: "1.2.1"),
 		.package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.0"),
 		.package(url: "https://github.com/apple/swift-collections.git", from: "1.1.4"),
 		.package(url: "https://github.com/autoreleasefool/swift-utilities.git", from: "3.0.0"),
