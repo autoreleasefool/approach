b364ac29b2f65fb73904238cd22d8d2d610fe13b
Joseph Roque <contact@josephroque.ca>
2025-03-24 23:59:42 -0700

    chore(ios): bump swift-composable-architecture to 1.18.0

--- ios/Approach/Package.swift
+++ ios/Approach/Package.swift
@@ -162,7 +162,7 @@ let package = Package(
 		.package(url: "https://github.com/autoreleasefool/swift-utilities.git", from: "3.0.0"),
 		.package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.48.0"),
 		.package(url: "https://github.com/groue/GRDB.swift.git", from: "7.4.0"),
-		.package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.17.1"),
+		.package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.18.0"),
 		.package(url: "https://github.com/pointfreeco/swift-concurrency-extras.git", from: "1.3.1"),
 		.package(url: "https://github.com/pointfreeco/swift-dependencies.git", from: "1.6.3"),
 		.package(url: "https://github.com/pointfreeco/swift-identified-collections.git", from: "1.1.0"),
