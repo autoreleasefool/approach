357c3df2729a3961c5dfc1c12662e25dc5597581
Joseph Roque <contact@josephroque.ca>
2025-03-28 13:32:55 -0700

    feat(android): AchievementsRepository

--- android/core/achievements/src/main/java/ca/josephroque/bowlingcompanion/core/achievements/earnable/ApproachAchievement.kt
+++ android/core/achievements/src/main/java/ca/josephroque/bowlingcompanion/core/achievements/earnable/ApproachAchievement.kt
@@ -9,7 +9,9 @@ object ApproachAchievement : EarnableAchievement {
 	override val events: List<ConsumableAchievementEvent>
 		get() = listOf(BasicAchievementEarned)
 
-	override fun consume(events: List<ConsumableAchievementEvent>): Pair<List<EarnableAchievement>, List<ConsumableAchievementEvent>> {
+	override fun consume(
+		events: List<ConsumableAchievementEvent>,
+	): Pair<List<EarnableAchievement>, List<ConsumableAchievementEvent>> {
 		val consumed = events.filter { it in this.events }
 		return Pair(consumed.map { this }, consumed)
 	}
---
+++ android/core/data/src/main/java/ca/josephroque/bowlingcompanion/core/data/repository/AchievementsRepository.kt
@@ -0,0 +1,14 @@
+package ca.josephroque.bowlingcompanion.core.data.repository
+
+import ca.josephroque.bowlingcompanion.core.achievements.ConsumableAchievementEvent
+import ca.josephroque.bowlingcompanion.core.model.Achievement
+import ca.josephroque.bowlingcompanion.core.model.AchievementListItem
+import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.Instant
+
+interface AchievementsRepository {
+	fun getEarnedAchievements(): Flow<List<AchievementListItem>>
+	fun getLatestAchievement(startDate: Instant): Flow<Achievement>
+
+	suspend fun insertEvent(event: ConsumableAchievementEvent)
+}
