0ded8b0c82d7ebae9c6be1465b33c796b9712d06
Joseph Roque <contact@josephroque.ca>
2025-05-25 21:34:07 -0700

    feat(ios): update dependency acknowledgements

--- ios/Approach/Sources/ConstantsLibrary/Acknowledgements.swift
+++ ios/Approach/Sources/ConstantsLibrary/Acknowledgements.swift
@@ -24,16 +24,21 @@ extension Acknowledgement {
 
 extension Acknowledgement {
 	public static let all: [Acknowledgement] = [
-		.init(name: "CocoaLumberjack", license: .init(fileName: "CocoaLumberjack", type: "txt")),
+		.init(name: "Concurrency Extras", license: .init(fileName: "ConcurrencyExtras", type: "txt")),
+		.init(name: "Dependencies", license: .init(fileName: "Dependencies", type: "txt")),
 		.init(name: "GRDB.swift", license: .init(fileName: "GRDB", type: "txt")),
+		.init(name: "Identified Collections", license: .init(fileName: "IdentifiedCollections", type: "txt")),
+		.init(name: "Issue Reporting", license: .init(fileName: "IssueReporting", type: "txt")),
+		.init(name: "Sentry", license: .init(fileName: "Sentry", type: "txt")),
+		.init(name: "Sharing", license: .init(fileName: "Sharing", type: "txt")),
 		.init(name: "SnapshotTesting", license: .init(fileName: "SnapshotTesting", type: "txt")),
 		.init(name: "Swift Algorithms", license: .init(fileName: "SwiftAlgorithms", type: "txt")),
 		.init(name: "Swift Async Algorithms", license: .init(fileName: "SwiftAsyncAlgorithms", type: "txt")),
-		.init(name: "Swift Dependencies", license: .init(fileName: "SwiftDependencies", type: "txt")),
-		.init(name: "Swift Identified Collections", license: .init(fileName: "SwiftIdentifiedCollections", type: "txt")),
+		.init(name: "Swift Collections", license: .init(fileName: "SwiftCollections", type: "txt")),
+		.init(name: "TelemetryDeck", license: .init(fileName: "TelemetryDeck", type: "txt")),
 		.init(name: "The Composable Architecture", license: .init(fileName: "ComposableArchitecture", type: "txt")),
+		.init(name: "ToastUI", license: .init(fileName: "ToastUI", type: "txt")),
 		.init(name: "WiggleAnimationModifier", license: .init(fileName: "WiggleAnimationModifier", type: "txt")),
-		.init(name: "XCTestDynamicOverlay", license: .init(fileName: "XCTestDynamicOverlay", type: "txt")),
 		.init(name: "ZIPFoundation", license: .init(fileName: "ZIPFoundation", type: "txt")),
 	]
 }
