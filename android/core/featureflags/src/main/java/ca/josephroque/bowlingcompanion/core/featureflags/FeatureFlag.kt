a22211757225c29832feb2553c08d1cb6832c8d5
Joseph Roque <contact@josephroque.ca>
2025-09-03 21:57:15 -0700

    feat(android): enable REORDER_GAMES feature flag

--- android/core/featureflags/src/main/java/ca/josephroque/bowlingcompanion/core/featureflags/FeatureFlag.kt
+++ android/core/featureflags/src/main/java/ca/josephroque/bowlingcompanion/core/featureflags/FeatureFlag.kt
@@ -30,7 +30,7 @@ enum class FeatureFlag(
 	MANUAL_TEAM_SERIES_FORM("ManualTeamSeriesForm", "2024-08-30", RolloutStage.DEVELOPMENT),
 	MOVING_SERIES_BETWEEN_LEAGUES("MovingSeriesBetweenLeagues", "2025-06-22", RolloutStage.RELEASE),
 	PRE_BOWL_FORM("PreBowlForm", "2024-03-24", RolloutStage.RELEASE),
-	REORDER_GAMES("ReorderGames", "2025-07-13", RolloutStage.DEVELOPMENT),
+	REORDER_GAMES("ReorderGames", "2025-07-13", RolloutStage.RELEASE),
 	SHARING_GAMES("SharingGames", "2024-11-03", RolloutStage.RELEASE),
 	SHARING_SERIES("SharingSeries", "2024-11-03", RolloutStage.RELEASE),
 	TEAMS("Teams", "2024-08-16", RolloutStage.RELEASE),
